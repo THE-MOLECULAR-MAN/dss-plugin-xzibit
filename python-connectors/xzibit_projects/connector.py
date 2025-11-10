c4784adda3d9f999fd00527386c9ee6c122ee4ef
admin <tim.honker@dataiku.com>
2025-11-10 08:52:58 -0500

    Edited file 'python-connectors/xzibit_projects/connector.py'

--- python-connectors/xzibit_projects/connector.py
+++ python-connectors/xzibit_projects/connector.py
@@ -64,7 +64,7 @@ class MyConnector(Connector):
             pprint.pprint(project_info)
             next_project = flatten_dict(project_info, 
                                include_keys=['projectKey', 'ownerLogin', 'projectStatus', 'contributors', 'name', 'projectLocation', 'projectStatus', 'shortDesc', 'tags', 'versionTag.lastModifiedOn', 'tutorialProject'])
-            # next_project = remove_prefix_from_keys(next_project, 'meta.')
+            next_project = remove_prefix_from_keys(next_project, 'versionTag.')
             # project_handle = self.client.get_project(next_project['id'])
             yield next_project
 
