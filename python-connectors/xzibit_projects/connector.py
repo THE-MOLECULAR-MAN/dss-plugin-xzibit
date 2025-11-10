381269fed8be4adcf9d67b8e1f981b1f18f2974b
admin <tim.honker@dataiku.com>
2025-11-10 08:47:40 -0500

    Edited file 'python-connectors/xzibit_projects/connector.py'

--- python-connectors/xzibit_projects/connector.py
+++ python-connectors/xzibit_projects/connector.py
@@ -1,5 +1,7 @@
 # This file is the actual code for the custom Python dataset xzibit_projects
 
+import pprint
+
 from dataiku import api_client
 from dataiku.connector import Connector
 from xzibit.utils import get_values_from_list_of_dicts, flatten_dict, remove_prefix_from_keys, get_values_for_key
