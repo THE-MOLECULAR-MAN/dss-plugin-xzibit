798357a7fab334358aca13a767d9f70fbe2435b2
admin <tim.honker@dataiku.com>
2025-11-10 10:06:26 -0500

    Edited file 'python-connectors/xzibit_connections/connector.py'

--- python-connectors/xzibit_connections/connector.py
+++ python-connectors/xzibit_connections/connector.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 from dataiku import api_client
 from dataiku.connector import Connector
-from xzibit.utils import get_values_from_list_of_dicts, flatten_dict, remove_prefix_from_keys, get_values_for_key
+from xzibit.utils import get_values_from_list_of_dicts, flatten_dict, remove_prefix_from_keys, get_values_for_key, pp
 
 """
 A custom Python dataset is a subclass of Connector.
