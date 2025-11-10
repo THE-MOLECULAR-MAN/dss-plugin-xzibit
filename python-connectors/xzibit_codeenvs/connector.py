fa09a5ce68d8425a8ad5e36bf62acdab93827475
admin <tim.honker@dataiku.com>
2025-11-10 11:09:48 -0500

    Edited file 'python-connectors/xzibit_codeenvs/connector.py'

--- python-connectors/xzibit_codeenvs/connector.py
+++ python-connectors/xzibit_codeenvs/connector.py
@@ -65,7 +65,7 @@ class MyConnector(Connector):
             code_env_handle = self.client.get_code_env(env_lang, env_name)
             settings = code_env_handle.get_settings().get_raw()
             next_code_env['corePackagesSet'] = settings.get('desc',[]).get('corePackagesSet',[])
-            next_code_env['path']            = settings.get('path',None)
+            next_code_env['path']            = settings.get('path', None)
             
             pp(settings)
             
