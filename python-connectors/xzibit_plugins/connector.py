a32d41985a9828571447b859dc0c32ef6e52a9b7
admin <tim.honker@dataiku.com>
2025-11-10 12:20:24 -0500

    Edited file 'python-connectors/xzibit_plugins/connector.py'

--- python-connectors/xzibit_plugins/connector.py
+++ python-connectors/xzibit_plugins/connector.py
@@ -57,8 +57,9 @@ class MyConnector(Connector):
             next_plugin = flatten_dict(plugin_info, 
                                include_keys=['meta.label', 'id', 'version', 'meta.author', 'meta.tags', 'meta.description', 'isDev'])
             next_plugin = remove_prefix_from_keys(next_plugin, 'meta.')
-            plugin_handle = self.client.get_plugin(next_plugin['id'])
             try:
+                plugin_handle = self.client.get_plugin(next_plugin['id'])
+
                 raw =  plugin_handle.list_usages().get_raw()
                 # pp(raw)
 
