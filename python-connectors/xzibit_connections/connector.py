092b28f44c6cbe0e0a3461179740d6756cfb48ff
admin <tim.honker@dataiku.com>
2025-11-10 10:08:03 -0500

    Edited file 'python-connectors/xzibit_connections/connector.py'

--- python-connectors/xzibit_connections/connector.py
+++ python-connectors/xzibit_connections/connector.py
@@ -38,7 +38,7 @@ class MyConnector(Connector):
         The main reading method.
         """
         
-        for connection_info in self.client.list_connections():
+        for connection_info in self.client.list_connections(as_type='listitems')):
             next_connection = flatten_dict(connection_info, 
                                include_keys=['connectionKey', 'ownerLogin', 'connectionStatus', 'contributors', 'name', 'connectionLocation', 'connectionStatus', 'shortDesc', 'tags', 'versionTag.lastModifiedOn', 'tutorialconnection'])
             #next_connection = remove_prefix_from_keys(next_connection, 'versionTag.')
