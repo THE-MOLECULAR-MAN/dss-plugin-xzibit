746ac29436e2bc4c94013171c8f77ab2bac67107
admin <tim.honker@dataiku.com>
2025-11-11 15:51:42 +0000

    Edited file 'python-connectors/xzibit_codeenvs/connector.py'

--- python-connectors/xzibit_codeenvs/connector.py
+++ python-connectors/xzibit_codeenvs/connector.py
@@ -54,7 +54,7 @@ class ConnectorCodeEnvs(Connector):
             except Exception as e:
                 print(f"Exception {e} with code_env_info:")
                 pp(code_env_info)
-                
+
             finally:
                 # return a single row
                 yield next_row
