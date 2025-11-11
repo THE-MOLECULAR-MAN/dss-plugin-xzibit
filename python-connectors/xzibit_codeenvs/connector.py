380c15f16369008e1f76f36a27598d2183971c71
admin <tim.honker@dataiku.com>
2025-11-11 15:47:52 +0000

    Edited file 'python-connectors/xzibit_codeenvs/connector.py'

--- python-connectors/xzibit_codeenvs/connector.py
+++ python-connectors/xzibit_codeenvs/connector.py
@@ -1,3 +1,82 @@
+####################################################################
+# Same imports for all dataset Classes
+####################################################################
+from dataiku import api_client
+from dataiku.connector import Connector
+from xzibit.utils import get_values_from_list_of_dicts, flatten_dict, remove_prefix_from_keys, get_values_for_key
+from xzibit.utils import pp
+
+
+####################################################################
+# Unique imports for this Class
+####################################################################
+
+class ConnectorCodeEnvs(Connector):
+
+    ####################################################################
+    # Code that has to be customized for this specific class
+    ####################################################################
+    def __init__(self, config, plugin_config):
+        Connector.__init__(self, config, plugin_config)
+        
+        self.client = api_client()
+        self.unique_id_key_name = ''
+        self.keys   = [self.unique_id_key_name, ]
+        self.objects_list = self.client.()
+
+            
+    def generate_rows(self, dataset_schema=None, dataset_partitioning=None,
+                            partition_id=None, records_limit = -1):
+        
+        # iterate through each object
+        for item_info in self.objects_list:
+            next_row = flatten_dict(item_info, include_keys=self.keys)
+            
+            # custom things for this specific class:
+            
+            # return a single row
+            yield next_row
+
+            
+####################################################################
+# Same for all instances:
+####################################################################
+    def get_records_count(self, partitioning=None, partition_id=None):
+        return len(self.objects_list)
+
+####################################################################
+# Intentionally not implemented, not needed for this type
+####################################################################
+    def get_partitioning(self):
+        raise NotImplementedError
+
+    def list_partitions(self, partitioning):
+        return []
+
+    def partition_exists(self, partitioning, partition_id):
+        raise NotImplementedError
+
+    def get_read_schema(self):
+        return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # This file is the actual code for the custom Python dataset dss-internals_code-envs
 
 # import the base class for the custom dataset
