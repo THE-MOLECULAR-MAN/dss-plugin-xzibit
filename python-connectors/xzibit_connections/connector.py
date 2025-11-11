037c268174a3ee527d596a69f53a4dc478c69359
admin <tim.honker@dataiku.com>
2025-11-11 15:30:55 +0000

    Edited file 'python-connectors/xzibit_connections/connector.py'

--- python-connectors/xzibit_connections/connector.py
+++ python-connectors/xzibit_connections/connector.py
@@ -1,3 +1,72 @@
+####################################################################
+# Same imports for all dataset Classes
+####################################################################
+from dataiku import api_client
+from dataiku.connector import Connector
+from xzibit.utils import get_values_from_list_of_dicts, flatten_dict, remove_prefix_from_keys, get_values_for_key
+from xzibit.utils import pp
+
+
+####################################################################
+# Unique imports for this Class
+####################################################################
+from datetime import datetime
+
+class ConnectorConnections(Connector):
+
+    ####################################################################
+    # Code that has to be customized for this specific class
+    ####################################################################
+    def __init__(self, config, plugin_config):
+        Connector.__init__(self, config, plugin_config)
+        
+        self.client = api_client()
+        self.unique_id_key_name = 'projectKey'
+        self.keys   = [self.unique_id_key_name, 'ownerLogin', 'projectStatus', 'contributors', 'name', 
+            'projectLocation', 'projectStatus', 'shortDesc', 
+            'tags', 'versionTag.lastModifiedOn', 'tutorialProject']
+        self.objects_list = self.client.list_projects()
+
+            
+    def generate_rows(self, dataset_schema=None, dataset_partitioning=None,
+                            partition_id=None, records_limit = -1):
+        
+        # iterate through each object
+        for item_info in self.objects_list:
+            next_row = flatten_dict(item_info, include_keys=self.keys)
+            
+            # custom things for this specific class:
+            next_row = remove_prefix_from_keys(next_row, 'versionTag.')
+            next_row['lastModifiedOn'] = datetime.fromtimestamp(next_row['lastModifiedOn'] // 1000)
+            
+            # return a single row
+            yield next_row
+
+            
+####################################################################
+# Same for all instances:
+####################################################################
+    def get_records_count(self, partitioning=None, partition_id=None):
+        return len(self.objects_list)
+
+####################################################################
+# Intentionally not implemented, not needed for this type
+####################################################################
+    def get_partitioning(self):
+        raise NotImplementedError
+
+    def list_partitions(self, partitioning):
+        return []
+
+    def partition_exists(self, partitioning, partition_id):
+        raise NotImplementedError
+
+    def get_read_schema(self):
+        return None
+
+    
+
+
 # This file is the actual code for the custom Python dataset xzibit_connections
 from datetime import datetime
 
