477a90e0b40783169ff34d5fec6532ff6ab999bf
admin <tim.honker@dataiku.com>
2025-11-11 17:57:31 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -39,10 +39,11 @@ class ConnectorRecipes(Connector):
         
         # iterate through each object
         for pk, proj_recipes in self.objects_list.items():
+            project_handle = self.client.get_project(pk)
 
             for r in proj_recipes:
                 recipe_id = r.id
-                recipe_handle = project.get_recipe(recipe_id)
+                recipe_handle = project_handle.get_recipe(recipe_id)
                 raw_data = recipe_handle.get_settings().get_recipe_raw_definition()
                 print(str(type(raw_data))) # dataikuapi.dss.recipe.SyncRecipeSettings
                 pp(raw_data)
