aa727011f939599954b762d2601b925999f6e37f
admin <tim.honker@dataiku.com>
2025-11-11 15:27:29 +0000

    Edited file 'python-connectors/xzibit_apps/connector.py'

--- python-connectors/xzibit_apps/connector.py
+++ python-connectors/xzibit_apps/connector.py
@@ -35,10 +35,6 @@ class ConnectorProjects(Connector):
         for item_info in self.objects_list:
             next_row = flatten_dict(item_info, include_keys=self.keys)
             
-            # custom things for this specific class:
-            next_row = remove_prefix_from_keys(next_row, 'versionTag.')
-            next_row['lastModifiedOn'] = datetime.fromtimestamp(next_row['lastModifiedOn'] // 1000)
-            
             # return a single row
             yield next_row
 
