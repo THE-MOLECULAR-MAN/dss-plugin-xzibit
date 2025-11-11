237849b2be5655847578c05b1af93012ed8b3815
admin <tim.honker@dataiku.com>
2025-11-11 18:09:59 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -52,7 +52,7 @@ class ConnectorRecipes(Connector):
                 
                 next_row = {'projectKey': pk,
                             'id': recipe_id,
-                           'type': recipe['type'],
+                           'type': raw_data['type'],
                            #'name': recipe.name,
                            # 'output_dataset': recipe.get('outputs',None).get('main',None).get('items',None).get('ref'),
                             #'engineType': recipe['params']['engineType']
