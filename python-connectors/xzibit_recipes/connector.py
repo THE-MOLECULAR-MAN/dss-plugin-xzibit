5f50c47fab4d3f6136e26f4a44603cbcba4d0ce3
admin <tim.honker@dataiku.com>
2025-11-11 23:44:27 +0000

    Edited files

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -41,6 +41,8 @@ class ConnectorRecipes(Connector):
                 recipe_settings_handle = recipe_handle.get_settings()
                 raw_data = recipe_settings_handle.get_recipe_raw_definition()
                 
+                if 'params' in raw_data and 
+                
                 next_row = {
                             'projectKey': pk,
                             'type': raw_data['type'],
