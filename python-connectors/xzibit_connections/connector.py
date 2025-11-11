57cc98e275f41c3fc0bc8cbd8e14b48d73bb6a5c
admin <tim.honker@dataiku.com>
2025-11-11 15:32:22 +0000

    Edited file 'python-connectors/xzibit_connections/connector.py'

--- python-connectors/xzibit_connections/connector.py
+++ python-connectors/xzibit_connections/connector.py
@@ -62,86 +62,86 @@ class ConnectorConnections(Connector):
     
 
 
-# This file is the actual code for the custom Python dataset xzibit_connections
-from datetime import datetime
+# # This file is the actual code for the custom Python dataset xzibit_connections
+# from datetime import datetime
 
-# import pprint
+# # import pprint
 
-from dataiku import api_client
-from dataiku.connector import Connector
-from xzibit.utils import get_values_from_list_of_dicts, flatten_dict, remove_prefix_from_keys, get_values_for_key, pp
+# from dataiku import api_client
+# from dataiku.connector import Connector
+# from xzibit.utils import get_values_from_list_of_dicts, flatten_dict, remove_prefix_from_keys, get_values_for_key, pp
 
-"""
-A custom Python dataset is a subclass of Connector.
+# """
+# A custom Python dataset is a subclass of Connector.
 
-The parameters it expects and some flags to control its handling by DSS are
-specified in the connector.json file.
+# The parameters it expects and some flags to control its handling by DSS are
+# specified in the connector.json file.
 
-Note: the name of the class itself is not relevant.
-"""
+# Note: the name of the class itself is not relevant.
+# """
 
-class MyConnector(Connector):
+# class MyConnector(Connector):
 
-    def __init__(self, config, plugin_config):
-        """
-        Constructor
-        """
-        Connector.__init__(self, config, plugin_config)  # pass the parameters to the base class
-        self.client = api_client()
+#     def __init__(self, config, plugin_config):
+#         """
+#         Constructor
+#         """
+#         Connector.__init__(self, config, plugin_config)  # pass the parameters to the base class
+#         self.client = api_client()
     
 
-    def get_read_schema(self):
-        """
-        """
-        return None
+#     def get_read_schema(self):
+#         """
+#         """
+#         return None
 
 
-    def generate_rows(self, dataset_schema=None, dataset_partitioning=None,
-                            partition_id=None, records_limit = -1):
-        """
-        The main reading method.
-        """
-        keys = ['name', 'type', 'usableBy', 'allowWrite', 'allowedGroups', 
-                'credentialsMode', 'name', 'type', 'usableBy']
-        for connection_info in self.client.list_connections(as_type='listitems'):
-            try:
-                next_row = flatten_dict(connection_info, include_keys=keys)
-            except Exception as e:
-                print(f"Exception {e} with connection_info:")
-                pp(connection_info)
-                next_row = list_to_error_dict(keys)
-                next_row['name'] = connection_info.get('name', 'NO_NAME')
-            finally:
-                yield next_row
+#     def generate_rows(self, dataset_schema=None, dataset_partitioning=None,
+#                             partition_id=None, records_limit = -1):
+#         """
+#         The main reading method.
+#         """
+#         keys = ['name', 'type', 'usableBy', 'allowWrite', 'allowedGroups', 
+#                 'credentialsMode', 'name', 'type', 'usableBy']
+#         for connection_info in self.client.list_connections(as_type='listitems'):
+#             try:
+#                 next_row = flatten_dict(connection_info, include_keys=keys)
+#             except Exception as e:
+#                 print(f"Exception {e} with connection_info:")
+#                 pp(connection_info)
+#                 next_row = list_to_error_dict(keys)
+#                 next_row['name'] = connection_info.get('name', 'NO_NAME')
+#             finally:
+#                 yield next_row
 
 
-    def get_partitioning(self):
-        """
-        Return the partitioning schema that the connector defines.
-        """
-        raise NotImplementedError
+#     def get_partitioning(self):
+#         """
+#         Return the partitioning schema that the connector defines.
+#         """
+#         raise NotImplementedError
 
 
-    def list_partitions(self, partitioning):
-        """Return the list of partitions for the partitioning scheme
-        passed as parameter"""
-        return []
+#     def list_partitions(self, partitioning):
+#         """Return the list of partitions for the partitioning scheme
+#         passed as parameter"""
+#         return []
 
 
-    def partition_exists(self, partitioning, partition_id):
-        """Return whether the partition passed as parameter exists
+#     def partition_exists(self, partitioning, partition_id):
+#         """Return whether the partition passed as parameter exists
 
-        Implementation is only required if the corresponding flag is set to True
-        in the connector definition
-        """
-        raise NotImplementedError
+#         Implementation is only required if the corresponding flag is set to True
+#         in the connector definition
+#         """
+#         raise NotImplementedError
 
 
-    def get_records_count(self, partitioning=None, partition_id=None):
-        """
-        Returns the count of records for the dataset (or a partition).
+#     def get_records_count(self, partitioning=None, partition_id=None):
+#         """
+#         Returns the count of records for the dataset (or a partition).
 
-        Implementation is only required if the corresponding flag is set to True
-        in the connector definition
-        """
-        return len(self.client.list_connections(as_type='listitems'))
+#         Implementation is only required if the corresponding flag is set to True
+#         in the connector definition
+#         """
+#         return len(self.client.list_connections(as_type='listitems'))
