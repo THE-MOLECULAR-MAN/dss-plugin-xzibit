2d8d1953610bff6c5df43ffd5d5c4b55deda3e66
admin <tim.honker@dataiku.com>
2025-11-11 17:59:21 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -47,15 +47,15 @@ class ConnectorRecipes(Connector):
                 raw_data = recipe_handle.get_settings().get_recipe_raw_definition()
                 
                 pp(raw_data)
-                #next_row = flatten_dict(raw_data, include_keys=self.keys)
+                next_row = flatten_dict(raw_data, include_keys=self.keys)
                 
-                next_row = {'projectKey': pk,
-                            'id': recipe_id,
-                           #'type': recipe['type'],
-                           #'name': recipe.name,
-                           # 'output_dataset': recipe.get('outputs',None).get('main',None).get('items',None).get('ref'),
-                            #'engineType': recipe['params']['engineType']
-                           }
+#                 next_row = {'projectKey': pk,
+#                             'id': recipe_id,
+#                            #'type': recipe['type'],
+#                            #'name': recipe.name,
+#                            # 'output_dataset': recipe.get('outputs',None).get('main',None).get('items',None).get('ref'),
+#                             #'engineType': recipe['params']['engineType']
+#                            }
                 pp(next_row)
                 # return a single row
                 yield next_row
