6c4afc456dc0132901474a7c77b9ccbe9306c17a
admin <tim.honker@dataiku.com>
2025-11-11 17:38:02 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -22,7 +22,7 @@ class ConnectorRecipes(Connector):
         
         self.client = api_client()
         self.unique_id_key_name = 'name'
-        self.keys   = [self.unique_id_key_name, 'name', ]
+        self.keys   = [self.unique_id_key_name, 'type', 'params.engineType']
         self.projectkeys = self.client.list_project_keys()
         self.objects_list = {}
         
