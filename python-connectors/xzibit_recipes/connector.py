8ea928dac137ae620685a4c3dc530002f0e2ac46
admin <tim.honker@dataiku.com>
2025-11-11 18:13:17 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -55,7 +55,7 @@ class ConnectorRecipes(Connector):
                             'type': raw_data['type'],
                             'name': recipe_handle.name,
                             'tags': raw_data['tags'],
-                            'lastModifiedOn': raw_data.get('versionTag',None).get('lastModifiedOn',None),
+                            # 'lastModifiedOn': raw_data.get('versionTag',None).get('lastModifiedOn',None),
                     
                 }
                 if 'params.basename' in next_row:
