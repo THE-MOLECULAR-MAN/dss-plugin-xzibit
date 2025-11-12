f73060836cce8a410495976074ced1846594a190
admin <tim.honker@dataiku.com>
2025-11-12 03:38:43 +0000

    Edited file 'python-connectors/xzibit_users/connector.py'

--- python-connectors/xzibit_users/connector.py
+++ python-connectors/xzibit_users/connector.py
@@ -33,7 +33,7 @@ class ConnectorUsers(Connector):
         # iterate through each object
         for item_info in self.objects_list:
             next_row = flatten_dict(item_info, include_keys=self.keys)
-            
+            pp(item_info)
             # return a single row
             yield next_row
 
