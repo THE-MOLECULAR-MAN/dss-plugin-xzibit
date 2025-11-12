39b822f37987563d1b5c1663ab537731060d95cd
admin <tim.honker@dataiku.com>
2025-11-12 18:23:33 +0000

    Edited file 'python-connectors/xzibit_datasets/connector.py'

--- python-connectors/xzibit_datasets/connector.py
+++ python-connectors/xzibit_datasets/connector.py
@@ -47,7 +47,7 @@ class ConnectorDatasets(Connector):
                     next_row = extract_nested_keys(raw_data, self.__keys)
                     
                     if next_row['type'] in ['PostgreSQL', 'Snowflake']:
-                        pp(raw_data)
+                        jd(raw_data)
 
 #                         #'data_steward': raw_data.get('data_steward', None),
 
