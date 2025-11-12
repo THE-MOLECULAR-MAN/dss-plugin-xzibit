ff77fb41d3dd2d08af756ff60ed8b0750dc806fb
admin <tim.honker@dataiku.com>
2025-11-12 17:10:54 +0000

    Edited file 'python-runnables/upgradeplugins/runnable.py'

--- python-runnables/upgradeplugins/runnable.py
+++ python-runnables/upgradeplugins/runnable.py
@@ -64,5 +64,5 @@ class MyRunnable(Runnable):
         The progress_callback is a function expecting 1 value: current progress
         """
         # raise Exception("unimplemented")
-        self.run()
+        self._upgrade_plugins()
         return None
