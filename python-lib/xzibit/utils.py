e1b21d753b351b6213e2189bc25f4880bc61380f
admin <tim.honker@dataiku.com>
2025-11-12 22:36:05 +0000

    Edited file 'python-lib/xzibit/utils.py'

--- python-lib/xzibit/utils.py
+++ python-lib/xzibit/utils.py
@@ -9,7 +9,7 @@ from json   import dumps  as jd
 def safe_extract_dataset_metadata(dataset_handle):
     """x"""
     print('safe_extract_dataset_metadata START')
-    assert isinstance(dataset, dataset_handle), f"Expected DSSDataset, got {type(dataset)}"        
+    assert isinstance(dataset, dataset_handle), f"Expected DSSDataset, got {type(dataset)}"
     try:
         dataset_metadata = {}
         # these should always work, even if dataset does not exist. These should not throw an exception:
