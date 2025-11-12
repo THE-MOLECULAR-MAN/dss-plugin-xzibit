9603b36e43adb50d0936c0ba294b8e10aea06edd
admin <tim.honker@dataiku.com>
2025-11-12 18:08:04 +0000

    Edited file 'python-connectors/xzibit_datasets/connector.py'

--- python-connectors/xzibit_datasets/connector.py
+++ python-connectors/xzibit_datasets/connector.py
@@ -41,7 +41,7 @@ class ConnectorDatasets(Connector):
                     dataset_settings_handle = dataset_handle.get_settings()
                     raw_data = dataset_settings_handle.get_raw()
 
-                    pp(raw_data)
+                    # pp(raw_data)
                     # params.connection
                     # len(metricsChecks.checks)
                     # len(schema.columns)
