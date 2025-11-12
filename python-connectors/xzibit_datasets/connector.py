5775ef255d905c3e96df7db077356b1dc5bb54c9
admin <tim.honker@dataiku.com>
2025-11-12 21:21:33 +0000

    Edited file 'python-connectors/xzibit_datasets/connector.py'

--- python-connectors/xzibit_datasets/connector.py
+++ python-connectors/xzibit_datasets/connector.py
@@ -48,7 +48,7 @@ class ConnectorDatasets(Connector):
                 try:
                     dataset_handle = project_handle.get_dataset(r.id)
                     if not dataset_handle.exists():
-                        print(f"Dataset does not exist: dataset {r.id} in project {pk}: Dataset:")
+                        print(f"Dataset does not exist: dataset {r.id} in project {pk}: Dataset metadata:")
                         # r is of type "dataikuapi.dss.dataset.DSSDataset"
                         md = r.get_metadata()
                         print(md)
