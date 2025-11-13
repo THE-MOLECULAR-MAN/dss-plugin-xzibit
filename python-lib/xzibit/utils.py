959b10aa85c7f6efb678671472c4006ff49a8175
admin <tim.honker@dataiku.com>
2025-11-13 04:03:44 +0000

    Edited file 'python-lib/xzibit/utils.py'

--- python-lib/xzibit/utils.py
+++ python-lib/xzibit/utils.py
@@ -43,6 +43,7 @@ def safe_extract_dataset_metadata(dataset_handle):
         # key_mapping.update(list_keys_recursive(raw_data)) # debugging, mapping out all the different keys depending on the type of dataset
 
         try:
+            # I NEED TO REWRITE KEYS?
             dataset_metadata_new = extract_nested_keys(raw_data, keys) # NOT causing exception
             # pp(dataset_metadata) # NOT causing exception
             #dataset_metadata = dataset_metadata | x # Python 3.9+  # maybe causing exception
