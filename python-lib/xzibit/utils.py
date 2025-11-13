4c67fb3a35e193cadb208c7860bb37b43226a298
admin <tim.honker@dataiku.com>
2025-11-13 03:40:44 +0000

    Edited file 'python-lib/xzibit/utils.py'

--- python-lib/xzibit/utils.py
+++ python-lib/xzibit/utils.py
@@ -54,6 +54,7 @@ def safe_extract_dataset_metadata(dataset_handle):
         dataset_metadata['column_names']       = [col["name"] for col in raw_data.get("schema", {}).get("columns", []) if "name" in col]
         dataset_metadata['creationTag.lastModifiedOn'] = int_to_datetime(dataset_metadata.get('creationTag.lastModifiedOn', None))
         dataset_metadata['versionTag.lastModifiedOn']  = int_to_datetime(dataset_metadata.get('versionTag.lastModifiedOn',  None))
+
     except DataikuException as e:
         dataset_metadata['exists'] = "EXCEPTION DataikuException"
         return dataset_metadata
