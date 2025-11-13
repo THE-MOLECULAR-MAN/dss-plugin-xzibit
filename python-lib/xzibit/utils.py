5bd8d08cc67e4c3973d263c2e7e5802f8c0b3384
admin <tim.honker@dataiku.com>
2025-11-13 03:48:58 +0000

    Edited file 'python-lib/xzibit/utils.py'

--- python-lib/xzibit/utils.py
+++ python-lib/xzibit/utils.py
@@ -53,7 +53,7 @@ def safe_extract_dataset_metadata(dataset_handle):
 
         # safe_extract_dataset_metadata - Generic exception 'NoneType' object has no attribute 'get'
         dataset_metadata['num_metrics_checks'] = len(raw_data.get('metricsChecks', {}).get('checks', []))
-#         dataset_metadata['num_columns']        = len(raw_data.get('schema').get('columns', []))
+#         dataset_metadata['num_columns']        = len(raw_data.get('schema', {}).get('columns', []))
 #         dataset_metadata['column_names']       = [col["name"] for col in raw_data.get("schema", {}).get("columns", []) if "name" in col]
 #         dataset_metadata['creationTag.lastModifiedOn'] = int_to_datetime(dataset_metadata.get('creationTag.lastModifiedOn', None))
 #         dataset_metadata['versionTag.lastModifiedOn']  = int_to_datetime(dataset_metadata.get('versionTag.lastModifiedOn',  None))
