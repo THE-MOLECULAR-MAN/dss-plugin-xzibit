dac808cb87276a153f540df270e2940976e71cd5
admin <tim.honker@dataiku.com>
2025-11-13 04:21:43 +0000

    Edited file 'python-lib/xzibit/utils.py'

--- python-lib/xzibit/utils.py
+++ python-lib/xzibit/utils.py
@@ -54,7 +54,6 @@ def safe_extract_dataset_metadata(dataset_handle, pk):
             print(f"safe_extract_dataset_metadata - EXCEPTION at extract_nested_keys")
             return dataset_metadata
        
-
         dataset_metadata['num_metrics_checks'] = len(raw_data.get('metricsChecks', {}).get('checks', []))
         dataset_metadata['num_columns']        = len(raw_data.get('schema', {}).get('columns', []))
         dataset_metadata['column_names']       = [col["name"] for col in raw_data.get("schema", {}).get("columns", []) if "name" in col]
