3cae54661518dd6355738c46e0866421a7c23bca
admin <tim.honker@dataiku.com>
2025-11-13 04:10:11 +0000

    Edited file 'python-lib/xzibit/utils.py'

--- python-lib/xzibit/utils.py
+++ python-lib/xzibit/utils.py
@@ -17,7 +17,7 @@ def safe_extract_dataset_metadata(dataset_handle):
     assert isinstance(dataset_handle, dataikuapi.dss.dataset.DSSDataset), f"safe_extract_dataset_metadata - Assertion failed: Expecting DSSDataset, got {type(dataset_handle)}"
     
     keys = ['name', 'type', 'formatType', 'params.connection',
-           'managed', 'params.mode', 'params.table', 'params.schema', 'params.database',
+           'managed', 'params.mode', 'params.table', 'params.schema', # 'params.database',
            'params.path', 
            'creationTag.lastModifiedBy.login', 'creationTag.lastModifiedOn',
            'versionTag.lastModifiedBy.login',  'versionTag.lastModifiedOn',
