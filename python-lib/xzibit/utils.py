ff50a200f5a4a94436250dd4d740ee30facdcd64
demo <tim.honker@dataiku.com>
2025-11-25 15:23:17 +0000

    Edited file 'python-lib/xzibit/utils.py'

--- python-lib/xzibit/utils.py
+++ python-lib/xzibit/utils.py
@@ -50,7 +50,8 @@ def get_dss_url_from_global_vars():
 
 
 def get_dss_base_url():
-    
+    url1 = get_dss_external_url()
+    if url:
     
 def safe_extract_dataset_metadata(dataset_handle, pk):
     """x"""
