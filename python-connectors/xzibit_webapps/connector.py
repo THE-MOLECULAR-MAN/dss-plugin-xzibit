fb363bbc062037b61ade50642ce9d0916aa290e9
demo <tim.honker@dataiku.com>
2025-11-25 21:24:35 +0000

    Edited file 'python-connectors/xzibit_webapps/connector.py'

--- python-connectors/xzibit_webapps/connector.py
+++ python-connectors/xzibit_webapps/connector.py
@@ -148,7 +148,7 @@ class ConnectorProjects(Connector):
                 {
                     "name":    "lastModifiedOn", 
                     "type":    "date",
-                    "meaning": "Date"
+                    "meaning": "DatetimeNoTz"
                 },
                 {
                     "name":    "last_session_activity", 
