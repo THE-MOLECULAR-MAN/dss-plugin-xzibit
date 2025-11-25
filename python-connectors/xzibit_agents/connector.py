b002002b8b5004f67e353fbbfb48a8e40ce9a0a9
demo <tim.honker@dataiku.com>
2025-11-25 16:11:20 +0000

    Edited file 'python-connectors/xzibit_agents/connector.py'

--- python-connectors/xzibit_agents/connector.py
+++ python-connectors/xzibit_agents/connector.py
@@ -105,7 +105,7 @@ class ConnectorProjects(Connector):
                         # FIX: Use raw_settings (from the full object) instead of agent_item.get_raw()
                         ## print("[generate_rows] raw_settings:")
                         pp(raw_settings)
-                        creation_user = raw_settings.get("creationTag", {}).get("user", "Unknown")
+                        creation_user = raw_settings.get("creationTag", {}).get("lastModifiedBy", {}).get("login","Unknown user")
                         
                         # creationTag.lastModifiedBy.login
                         # creationTag.lastModifiedBy.lastModifiedOn
