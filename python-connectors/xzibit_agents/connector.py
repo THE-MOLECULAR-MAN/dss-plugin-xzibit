a8f69fd66de2fafb1c0cc7cdec9b7924f0d1b428
demo <tim.honker@dataiku.com>
2025-11-25 15:51:47 +0000

    Edited file 'python-connectors/xzibit_agents/connector.py'

--- python-connectors/xzibit_agents/connector.py
+++ python-connectors/xzibit_agents/connector.py
@@ -89,6 +89,8 @@ class ConnectorProjects(Connector):
                         # creationTag.lastModifiedBy.login
                         # creationTag.lastModifiedBy.lastModifiedOn
                         agent_version = agent_item.get('activeVersion', 'Unknown')
+                        
+                        llm_vendor, llm_connection_name, llm_model = split(llm_model_id, ":")
                         next_row = {
                             "projectKey": project_key,
                             "Agent Name": agent_item.name,
