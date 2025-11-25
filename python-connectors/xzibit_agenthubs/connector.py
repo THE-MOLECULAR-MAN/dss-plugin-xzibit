fb6b68736bb3053a89cc4d403f48e7a80249243a
demo <tim.honker@dataiku.com>
2025-11-25 20:30:36 +0000

    Edited file 'python-connectors/xzibit_agenthubs/connector.py'

--- python-connectors/xzibit_agenthubs/connector.py
+++ python-connectors/xzibit_agenthubs/connector.py
@@ -94,7 +94,7 @@ class ConnectorProjects(Connector):
 ####################################################################
     def get_records_count(self, partitioning=None, partition_id=None):
         # return len(self.objects_list)
-        return None
+        return self.__count
 
 ####################################################################
 # Intentionally not implemented, not needed for this type
