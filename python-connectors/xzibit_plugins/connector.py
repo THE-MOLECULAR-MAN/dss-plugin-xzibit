bf3c1bd6c6d0ad1678e3d7ff6e7d1023ac5775ae
demo <tim.honker@dataiku.com>
2025-11-25 04:49:38 +0000

    Edited file 'python-connectors/xzibit_plugins/connector.py'

--- python-connectors/xzibit_plugins/connector.py
+++ python-connectors/xzibit_plugins/connector.py
@@ -51,6 +51,7 @@ class ConnectorPlugins(Connector):
                 next_row = list_to_error_dict(keys)
             finally:
                 yield next_row
+
     def get_read_schema(self):
         # Data types: https://developer.dataiku.com/latest/api-reference/python/datasets.html#dataiku.core.dataset.Schema
         # Meanings: Text, JSONArrayMeaning, Email, Boolean, DatetimeNoTz, Date, FreeText, LongMeaning
