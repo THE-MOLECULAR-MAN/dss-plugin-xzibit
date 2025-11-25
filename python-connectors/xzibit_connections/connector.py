ff80a669426b268e12f114725d3ef94248d7ba21
demo <tim.honker@dataiku.com>
2025-11-25 22:00:23 +0000

    Edited file 'python-connectors/xzibit_connections/connector.py'

--- python-connectors/xzibit_connections/connector.py
+++ python-connectors/xzibit_connections/connector.py
@@ -20,7 +20,7 @@ class ConnectorConnections(Connector):
         
         self.client = api_client()
         self.unique_id_key_name = 'name'
-        self.keys   = [self.unique_id_key_name, 'type', 'usableBy', 'allowWrite', 'allowedGroups', 
+        self.keys   = [self.unique_id_key_name, 'type', 'usableBy', 'allowWrite', #'allowedGroups', 
                 'credentialsMode', 'type', 'usableBy']
         self.objects_list = self.client.list_connections(as_type='listitems')
 
