0350b50a7a50a2cf8f04bc43b3862cb16ba328bf
demo <tim.honker@dataiku.com>
2025-11-25 20:15:45 +0000

    Edited file 'python-connectors/xzibit_agenthubs/connector.py'

--- python-connectors/xzibit_agenthubs/connector.py
+++ python-connectors/xzibit_agenthubs/connector.py
@@ -1,128 +1,85 @@
-# This file is the actual code for the custom Python dataset xzibit_agenthubs
-
-# import the base class for the custom dataset
-from six.moves import xrange
+####################################################################
+# Same imports for all dataset Classes
+####################################################################
+from dataiku import api_client
 from dataiku.connector import Connector
+from xzibit.utils import *
 
-"""
-A custom Python dataset is a subclass of Connector.
+####################################################################
+# Unique imports for this Class
+####################################################################
+from datetime import datetime
 
-The parameters it expects and some flags to control its handling by DSS are
-specified in the connector.json file.
+def get_agent_url(project_key, agent_id, agent_version): 
+    # https://honker-design-2.se-platform.dataiku-sandbox.io/projects/Data_Dictionary_and_DSS_Instance_datasets_test_project/savedmodels/0XpBITsO/agent/S-Data_Dictionary_and_DSS_Instance_datasets_test_project-0XpBITsO-v1
+    # https://honker-design-2.amer.dataiku-sandbox.io       /projects/Data_Dictionary_and_DSS_Instance_datasets_test_project/savedmodels/0XpBITsO/agent/S-Data_Dictionary_and_DSS_Instance_datasets_test_project-0XpBITsO-v1
+    base_url = get_dss_base_url()
+    return f"{base_url}/projects/{project_key}/savedmodels/{agent_id}/agent/S-{project_key}-{agent_id}-{agent_version}"  
 
-Note: the name of the class itself is not relevant.
-"""
-class MyConnector(Connector):
+def parse_llm_id(llm_string: str):
+    """
+    Splits a string by ':' into exactly 3 variables.
+    Returns None for missing fields.
+    """
+    if not llm_string:
+        return None, None, None
 
+    # Split the string
+    parts = llm_string.split(':')
+    
+    # Pad the list with None to ensure it has at least 3 elements, 
+    # then slice to take exactly the first 3.
+    # This handles cases with 1, 2, or 3+ segments gracefully.
+    padded = (parts + [None] * 3)[:3]
+    
+    return padded[0], padded[1], padded[2]
+
+
+class ConnectorProjects(Connector):
+
+    ####################################################################
+    # Code that has to be customized for this specific class
+    ####################################################################
     def __init__(self, config, plugin_config):
-        """
-        The configuration parameters set up by the user in the settings tab of the
-        dataset are passed as a json object 'config' to the constructor.
-        The static configuration parameters set up by the developer in the optional
-        file settings.json at the root of the plugin directory are passed as a json
-        object 'plugin_config' to the constructor
-        """
-        Connector.__init__(self, config, plugin_config)  # pass the parameters to the base class
+       #  print("[DEBUG agents] Constructor START")
+        Connector.__init__(self, config, plugin_config)
+        
+        self.__client = api_client()
+        #self.__unique_id_key_name = 'projectKey'
+        #self.__keys   = [self.unique_id_key_name, 'ownerLogin', 'projectStatus', 'contributors', 'name', 
+#             'shortDesc', 'description',
+#             'tags', 'versionTag.lastModifiedOn', 'tutorialProject']
+        self.__objects_list = self.__client.list_project_keys()
+        # print("[DEBUG agents] Constructor END")
 
-        # perform some more initialization
-        self.theparam1 = self.config.get("parameter1", "defaultValue")
+        
+    def generate_rows(self, dataset_schema=None, dataset_partitioning=None,
+                            partition_id=None, records_limit = -1):       
+        
 
+        # print("[generate_rows] END")
+                
+                
     def get_read_schema(self):
-        """
-        Returns the schema that this connector generates when returning rows.
-
-        The returned schema may be None if the schema is not known in advance.
-        In that case, the dataset schema will be infered from the first rows.
-
-        If you do provide a schema here, all columns defined in the schema
-        will always be present in the output (with None value),
-        even if you don't provide a value in generate_rows
-
-        The schema must be a dict, with a single key: "columns", containing an array of
-        {'name':name, 'type' : type}.
-
-        Example:
-            return {"columns" : [ {"name": "col1", "type" : "string"}, {"name" :"col2", "type" : "float"}]}
-
-        Supported types are: string, int, bigint, float, double, date, boolean
-        """
-
-        # In this example, we don't specify a schema here, so DSS will infer the schema
-        # from the columns actually returned by the generate_rows method
+        # Data types: https://developer.dataiku.com/latest/api-reference/python/datasets.html#dataiku.core.dataset.Schema
+        # Meanings: Text, JSONArrayMeaning, Email, Boolean, DatetimeNoTz, Date, FreeText
+        return None
+            
+####################################################################
+# Same for all instances:
+####################################################################
+    def get_records_count(self, partitioning=None, partition_id=None):
+        # return len(self.objects_list)
         return None
 
-    def generate_rows(self, dataset_schema=None, dataset_partitioning=None,
-                            partition_id=None, records_limit = -1):
-        """
-        The main reading method.
-
-        Returns a generator over the rows of the dataset (or partition)
-        Each yielded row must be a dictionary, indexed by column name.
-
-        The dataset schema and partitioning are given for information purpose.
-        """
-        for i in xrange(1,10):
-            yield { "first_col" : str(i), "my_string" : "Yes" }
-
-
-    def get_writer(self, dataset_schema=None, dataset_partitioning=None,
-                         partition_id=None, write_mode="OVERWRITE"):
-        """
-        Returns a writer object to write in the dataset (or in a partition).
-
-        The dataset_schema given here will match the the rows given to the writer below.
-
-        write_mode can either be OVERWRITE or APPEND.
-        It will not be APPEND unless the plugin explicitly supports append mode. See flag supportAppend in connector.json.
-        If applicable, the write_mode should be handled in the plugin code.
-
-        Note: the writer is responsible for clearing the partition, if relevant.
-        """
-        raise NotImplementedError
-
-
+####################################################################
+# Intentionally not implemented, not needed for this type
+####################################################################
     def get_partitioning(self):
-        """
-        Return the partitioning schema that the connector defines.
-        """
         raise NotImplementedError
-
 
     def list_partitions(self, partitioning):
-        """Return the list of partitions for the partitioning scheme
-        passed as parameter"""
         return []
 
-
     def partition_exists(self, partitioning, partition_id):
-        """Return whether the partition passed as parameter exists
-
-        Implementation is only required if the corresponding flag is set to True
-        in the connector definition
-        """
         raise NotImplementedError
-
-
-    def get_records_count(self, partitioning=None, partition_id=None):
-        """
-        Returns the count of records for the dataset (or a partition).
-
-        Implementation is only required if the corresponding flag is set to True
-        in the connector definition
-        """
-        raise NotImplementedError
-
-
-class CustomDatasetWriter(object):
-    def __init__(self):
-        pass
-
-    def write_row(self, row):
-        """
-        Row is a tuple with N + 1 elements matching the schema passed to get_writer.
-        The last element is a dict of columns not found in the schema
-        """
-        raise NotImplementedError
-
-    def close(self):
-        pass
