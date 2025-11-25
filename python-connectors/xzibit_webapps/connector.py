0293447f0b61dd2a39247016e892fdedad6dafca
demo <tim.honker@dataiku.com>
2025-11-25 20:39:08 +0000

    Edited file 'python-connectors/xzibit_webapps/connector.py'

--- python-connectors/xzibit_webapps/connector.py
+++ python-connectors/xzibit_webapps/connector.py
@@ -45,7 +45,7 @@ class ConnectorProjects(Connector):
                             "webapp_id": webapp.get("id",None),
                             "type": webapp.get("type"),
                             #"created_by_user": webapp.get("createdBy", {}).get("login"),
-                            #"backendRunning": webapp.get("backendRunning",None),
+                            "backendRunning": webapp.get("backendRunning",None),
                             #"url": get_agenthub_url(project_key, webapp.get("id","")),
                             #"created_on": datetime.fromtimestamp(webapp.get("createdOn", None) // 1000),
                             #"lastModifiedBy": webapp.get("lastModifiedBy", {}).get('login',None),
