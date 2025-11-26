cc291711d4ee86d65352b0244a8faaee30d12741
demo <tim.honker@dataiku.com>
2025-11-26 02:47:43 +0000

    Edited file 'python-connectors/xzibit_users/connector.py'

--- python-connectors/xzibit_users/connector.py
+++ python-connectors/xzibit_users/connector.py
@@ -60,7 +60,7 @@ class ConnectorUsers(Connector):
                 {"name": "login", "type": "string", "meaning": "Text"},
                 {"name": "displayName", "type": "string", "meaning": "Text"},
                 {"name": "userProfile", "type": "string", "meaning": "Text"},
-                {"name": "groups", "type": "array", "meaning": "JSONArrayMeaning"},
+                {"name": "groups", "type": "string", "meaning": "Text"},
                 {"name": "sourceType", "type": "string", "meaning": "Text"},
                 {"name": "email", "type": "string", "meaning": "Email"},
                 {"name": "enabled", "type": "boolean", "meaning": "Boolean"},
