92893467dfaa7c993b5d4d40bbe1a7c273b5247b
demo <tim.honker@dataiku.com>
2025-11-26 13:40:19 +0000

    Edited file 'python-connectors/xzibit_meanings/connector.py'

--- python-connectors/xzibit_meanings/connector.py
+++ python-connectors/xzibit_meanings/connector.py
@@ -5,6 +5,7 @@ from dataiku import api_client
 from dataiku.connector import Connector
 from xzibit.utils import *
 
+# Meanings don't have an obvious, dedicated URL
 
 class ConnectorMeanings(Connector):
     """TBD"""
