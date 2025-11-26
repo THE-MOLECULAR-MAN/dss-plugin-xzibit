d4fd02d759ff05fb59552e0b508eda054610cc86
demo <tim.honker@dataiku.com>
2025-11-26 13:34:35 +0000

    Edited file 'python-connectors/xzibit_codeenvs/connector.py'

--- python-connectors/xzibit_codeenvs/connector.py
+++ python-connectors/xzibit_codeenvs/connector.py
@@ -8,6 +8,7 @@ from xzibit.utils import *
 
 def get_codeenv_url(env_name, env_lang='python'):
     # https://beta-design.se-platform.dataiku-sandbox.io/admin/code-envs/design/python/brave-mcp/
+    # https://honker-design-2.se-platform.dataiku-sandbox.io/admin/code-envs/design/python/CausalModels
     try:
         base_url = get_dss_base_url()
         if base_url is None or env_lang is None or base_url is None:
