544c0f4e3a64114fec75fb2538c1e8be79541611
demo <tim.honker@dataiku.com>
2025-11-26 13:52:05 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -5,6 +5,7 @@ from dataiku import api_client
 from dataiku.connector import Connector
 from xzibit.utils import *
 
+# https://honker-design-2.se-platform.dataiku-sandbox.io/projects/PMMOPTIMIZINGOMNICHANNELMARKETINGLLM/recipes/compute_Product_sales_by_acc_joined/
 
 class ConnectorRecipes(Connector):
 
