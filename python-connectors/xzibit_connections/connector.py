f260cc6144623d515cac1bc53e194181215b9e04
demo <tim.honker@dataiku.com>
2025-11-26 00:11:51 +0000

    Edited file 'python-connectors/xzibit_connections/connector.py'

--- python-connectors/xzibit_connections/connector.py
+++ python-connectors/xzibit_connections/connector.py
@@ -42,7 +42,7 @@ class ConnectorConnections(Connector):
 # Same for all instances:
 ####################################################################
     def get_records_count(self, partitioning=None, partition_id=None):
-        return len(self.__client.list_connections(as_type='listitems'))
+        return len(self.__client.list_connections())
 
 ####################################################################
 # Intentionally not implemented, not needed for this type
