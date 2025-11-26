5a5e72799f46d96fa268b61e881dabbddd83f87e
admin <tim.honker@dataiku.com>
2025-11-26 18:24:02 +0000

    Edited file 'python-connectors/xzibit_users/connector.py'

--- python-connectors/xzibit_users/connector.py
+++ python-connectors/xzibit_users/connector.py
@@ -66,7 +66,7 @@ class ConnectorUsers(Connector):
                 next_row["creationDate"] = int_to_datetime(next_row["creationDate"])
                 # pp(item_info)
             except Exception:
-                    print("whatever")
+                print("whatever")
             finally
                 yield next_row
 
