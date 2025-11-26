38ee70fcee100b689fb26cb1bf5bbc628b76d9e9
demo <tim.honker@dataiku.com>
2025-11-26 01:23:00 +0000

    Edited file 'python-connectors/xzibit_meanings/connector.py'

--- python-connectors/xzibit_meanings/connector.py
+++ python-connectors/xzibit_meanings/connector.py
@@ -29,6 +29,7 @@ class ConnectorMeanings(Connector):
             next_row = flatten_dict(item_info, include_keys=self.keys)
             yield next_row
 
+
     def get_read_schema(self):
         # Data types: https://developer.dataiku.com/latest/api-reference/python/datasets.html#dataiku.core.dataset.Schema
         # Meanings: Text, JSONArrayMeaning, Email, Boolean, DatetimeNoTz, Date, FreeText, LongMeaning
