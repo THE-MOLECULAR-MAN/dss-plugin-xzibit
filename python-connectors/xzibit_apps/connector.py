adb511ebe5ac9ae712f9da0e770902898c7b2c80
demo <tim.honker@dataiku.com>
2025-11-26 00:19:04 +0000

    Edited file 'python-connectors/xzibit_apps/connector.py'

--- python-connectors/xzibit_apps/connector.py
+++ python-connectors/xzibit_apps/connector.py
@@ -30,7 +30,6 @@ class ConnectorApps(Connector):
 
     def generate_rows(self, dataset_schema=None, dataset_partitioning=None,
                             partition_id=None, records_limit = -1):
-        
         # iterate through each object
         for item_info in self.__objects_list:
             next_row = flatten_dict(item_info, include_keys=self.keys)
