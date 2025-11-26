2b3e625a1145c290f8cfa3921847aab0e07c4376
demo <tim.honker@dataiku.com>
2025-11-26 01:13:25 +0000

    Edited file 'python-connectors/xzibit_apps/connector.py'

--- python-connectors/xzibit_apps/connector.py
+++ python-connectors/xzibit_apps/connector.py
@@ -25,7 +25,6 @@ class ConnectorApps(Connector):
                 'onlyLimitedVisibility']
 
 
-
     def generate_rows(self, dataset_schema=None, dataset_partitioning=None,
                             partition_id=None, records_limit = -1):
         # iterate through each object
