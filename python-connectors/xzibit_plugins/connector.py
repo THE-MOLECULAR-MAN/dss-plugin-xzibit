79eccbb771aa3acace7e314af00a86eb17ad8113
demo <tim.honker@dataiku.com>
2025-11-26 13:40:58 +0000

    Edited file 'python-connectors/xzibit_plugins/connector.py'

--- python-connectors/xzibit_plugins/connector.py
+++ python-connectors/xzibit_plugins/connector.py
@@ -5,6 +5,7 @@ from dataiku import api_client
 from dataiku.connector import Connector
 from xzibit.utils import *
 
+# https://honker-design-2.se-platform.dataiku-sandbox.io/plugins/agent-connect/summary/
 
 class ConnectorPlugins(Connector):
 
