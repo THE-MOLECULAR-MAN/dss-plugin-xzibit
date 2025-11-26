5af4c9d9427650f470dbbb07a712f6c1e3f5202e
demo <tim.honker@dataiku.com>
2025-11-26 13:20:18 +0000

    Edited file 'python-connectors/xzibit_clusters/connector.py'

--- python-connectors/xzibit_clusters/connector.py
+++ python-connectors/xzibit_clusters/connector.py
@@ -3,7 +3,7 @@
 ####################################################################
 from dataiku import api_client
 from dataiku.connector import Connector
-from xzibit.utils import flatten_dict
+from xzibit.utils import *
 
 def get_cluster_url(cluster_id):
     # https://beta-design.se-platform.dataiku-sandbox.io/admin/clusters/k8s-gpu-small
