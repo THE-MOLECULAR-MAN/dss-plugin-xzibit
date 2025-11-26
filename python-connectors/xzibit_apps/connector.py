509d92f03117cbe651c8cd5a8dfc5d8a42127ee0
demo <tim.honker@dataiku.com>
2025-11-26 01:13:38 +0000

    Edited file 'python-connectors/xzibit_apps/connector.py'

--- python-connectors/xzibit_apps/connector.py
+++ python-connectors/xzibit_apps/connector.py
@@ -32,12 +32,13 @@ class ConnectorApps(Connector):
             next_row = flatten_dict(item_info, include_keys=self.__keys)
             yield next_row
 
+    def get_records_count(self, partitioning=None, partition_id=None):
+        return len(self.__client.list_apps())
+
             
 ####################################################################
 # Same for all instances:
 ####################################################################
-    def get_records_count(self, partitioning=None, partition_id=None):
-        return len(self.__client.list_apps())
 
 ####################################################################
 # Intentionally not implemented, not needed for this type
