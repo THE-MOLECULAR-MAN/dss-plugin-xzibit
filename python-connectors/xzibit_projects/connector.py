af0fc8edee4fb959da681b60acdde430c55e1c23
demo <tim.honker@dataiku.com>
2025-11-26 01:56:43 +0000

    Edited file 'python-connectors/xzibit_projects/connector.py'

--- python-connectors/xzibit_projects/connector.py
+++ python-connectors/xzibit_projects/connector.py
@@ -35,6 +35,7 @@ class ConnectorProjects(Connector):
             next_row['lastModifiedOn'] = datetime.fromtimestamp(next_row['lastModifiedOn'] // 1000)
             yield next_row
 
+            
     def get_read_schema(self):
         # Data types: https://developer.dataiku.com/latest/api-reference/python/datasets.html#dataiku.core.dataset.Schema
         # Meanings: Text, JSONArrayMeaning, Email, Boolean, DatetimeNoTz, Date, FreeText
