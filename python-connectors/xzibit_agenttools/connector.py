518df96b69577810d56f3efe1064230cd8939a18
admin <tim.honker@dataiku.com>
2025-12-03 19:27:26 +0000

    Edited file 'python-connectors/xzibit_agenttools/connector.py'

--- python-connectors/xzibit_agenttools/connector.py
+++ python-connectors/xzibit_agenttools/connector.py
@@ -97,7 +97,7 @@ class ConnectorProjects(Connector):
                         )
                         next_row["Last modified by user"] = last_modified_user
                         next_row["agent_tool_params"] = replace_empty_arrays_sets_with_none(raw_settings.get('params',None))
-                        next_row["agent_tool_LLMid"]  = raw_settings.get('params',{}).get("llmId", "BLAH")
+                        next_row["agent_tool_LLMid"]  = raw_settings.get('params',{}).get("llmId", "NOT_FOUND")
                         
                         # next_row["customFields"]   = raw_settings.get('customFields',None)
                         next_row["dkuProperties"]    = replace_empty_arrays_sets_with_none(raw_settings.get('dkuProperties',None))
