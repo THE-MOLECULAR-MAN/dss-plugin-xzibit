6348ce0c59bd357393bf52756c29f838c1748105
admin <tim.honker@dataiku.com>
2025-12-03 16:48:51 +0000

    Edited file 'python-connectors/xzibit_agenttools/connector.py'

--- python-connectors/xzibit_agenttools/connector.py
+++ python-connectors/xzibit_agenttools/connector.py
@@ -97,7 +97,7 @@ class ConnectorProjects(Connector):
                             .get("login", None)
                         )
                         next_row["Last modified by user"] = last_modified_user
-                        next_row["params"] = raw_settings.get('params',None)          
+                        next_row["agent_tool_params"] = raw_settings.get('params',None)          
 
 #                             "Active Version": active_version_id,
 #                             "Agent Version": agent_version,
