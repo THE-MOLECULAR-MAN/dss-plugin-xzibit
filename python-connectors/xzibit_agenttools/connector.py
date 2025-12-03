24949ebeeaef4b859109859d3b5f189ed23b530a
admin <tim.honker@dataiku.com>
2025-12-03 15:43:13 +0000

    Edited file 'python-connectors/xzibit_agenttools/connector.py'

--- python-connectors/xzibit_agenttools/connector.py
+++ python-connectors/xzibit_agenttools/connector.py
@@ -79,7 +79,7 @@ class ConnectorProjects(Connector):
 #                             "Agent Version": agent_version,
                             "tags": raw_settings.get("tags", None),
 #                             "Last Modified timestamp": last_modified_on,
-                            "url": get_agenttool_url(
+                            "url_dss_object": get_agenttool_url(
                                 project_key, agent_tool_id
                             ),
                         }
