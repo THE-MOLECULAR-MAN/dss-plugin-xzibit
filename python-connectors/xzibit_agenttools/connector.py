b2ed1ab64f109eed15b40a748c6c45d189acee84
admin <tim.honker@dataiku.com>
2025-12-03 15:08:24 +0000

    Edited file 'python-connectors/xzibit_agenttools/connector.py'

--- python-connectors/xzibit_agenttools/connector.py
+++ python-connectors/xzibit_agenttools/connector.py
@@ -57,9 +57,11 @@ class ConnectorProjects(Connector):
                         agent = project.get_agent_tool(agent_item.id)
                         settings = agent.get_settings() #  <dataikuapi.dss.agent_tool.DSSAgentToolSettings object at 0x7f90301a83d0>
                         # https://developer.dataiku.com/latest/api-reference/python/agents.html#dataikuapi.dss.agent_tool.DSSAgentToolSettings
+                        # get_raw
+                        
 
                         print(f"[agent_tools.generate_rows] settings:")
-                        pp(settings)
+                        pp(settings.get_raw())
                         # raw_settings = settings.get_raw()
 
                         next_row = {
