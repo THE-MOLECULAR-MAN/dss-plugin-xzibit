86c68a9b19bd33dc25fc72b10c78d93eb3c7aa78
admin <tim.honker@dataiku.com>
2025-12-03 21:09:50 +0000

    Edited file 'python-connectors/xzibit_agenttools/connector.py'

--- python-connectors/xzibit_agenttools/connector.py
+++ python-connectors/xzibit_agenttools/connector.py
@@ -154,6 +154,7 @@ class ConnectorProjects(Connector):
     
     def get_records_count(self, partitioning=None, partition_id=None):
         """TBD"""
+        print(f"xzibit_agenttools get_records_count ====================================")
         return self.__count
 
     ####################################################################
