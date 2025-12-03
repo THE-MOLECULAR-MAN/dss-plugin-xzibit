243008760164d9268b3e666334679c645aca6681
admin <tim.honker@dataiku.com>
2025-12-03 19:23:35 +0000

    Edited file 'python-lib/xzibit/utils.py'

--- python-lib/xzibit/utils.py
+++ python-lib/xzibit/utils.py
@@ -20,7 +20,7 @@ def replace_empty_arrays_sets_with_none(x):
             (isinstance(x, list) and len(x) == 0) or 
             (isinstance(x, str) and x == "{}") or 
             (isinstance(x, set) and len(x) == 0) or 
-            (isinstance(x, dict) and (not x):
+            (isinstance(x, dict) and (not x)):
         #if x is None or x in ["[]", "{}", "null"] or len(x) == 0:
             return None
     except Exception as e:
