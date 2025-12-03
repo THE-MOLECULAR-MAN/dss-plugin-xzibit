513909ff259d9ea15ba43df5dbe5cdc25f54e9e6
admin <tim.honker@dataiku.com>
2025-12-03 16:45:52 +0000

    Edited file 'python-connectors/xzibit_agenttools/connector.py'

--- python-connectors/xzibit_agenttools/connector.py
+++ python-connectors/xzibit_agenttools/connector.py
@@ -73,7 +73,7 @@ class ConnectorProjects(Connector):
                         next_row["agent_tool_description_for_LLM"] = raw_settings.get('additionalDescriptionForLLM',None)
                         next_row["tags"] = raw_settings.get("tags", [])
 #                         next_row["url"] =  get_agenttool_url(
-#                                 project_key, agent_tool_id
+#                                 project_key, next_row["agent_tool_id"]
 #                             )
 
                         creation_user = (
