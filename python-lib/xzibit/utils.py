86cbe5293d3de6f3cec788c6b8ab78721fe36ffa
admin <tim.honker@dataiku.com>
2025-12-03 18:48:17 +0000

    Edited file 'python-lib/xzibit/utils.py'

--- python-lib/xzibit/utils.py
+++ python-lib/xzibit/utils.py
@@ -18,6 +18,8 @@ def replace_empty_arrays_sets_with_none(x):
     # if (isinstance(x, str) and x == "[]") or (isinstance(x, list) and len(x) == 0) or (isinstance(x, str) and x == "{}") or (isinstance(x, set) and len(x) == 0):
     if x in ["[]", "{}"] or len(x) == 0:
         return None
+    exception:
+        return None
     return x
 
 
