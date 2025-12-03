b64e1f48a0ba666b1d7aa5281b5c28a6deb28ee5
admin <tim.honker@dataiku.com>
2025-12-03 18:34:05 +0000

    Edited file 'python-connectors/xzibit_agenttools/connector.py'

--- python-connectors/xzibit_agenttools/connector.py
+++ python-connectors/xzibit_agenttools/connector.py
@@ -118,7 +118,7 @@ class ConnectorProjects(Connector):
         # print("[generate_rows] END")
 
     def get_read_schema(self):
-        """TBD"""
+        """Returns the read schema for Agent Tools"""
         # Data types: https://developer.dataiku.com/latest/api-reference/python/datasets.html#dataiku.core.dataset.Schema
         # Meanings: Text, JSONArrayMeaning, Email, Boolean, DatetimeNoTz, Date, FreeText, LongMeaning
         return {'columns': [{'meaning': 'Text', 'name': 'projectKey', 'type': 'string'},
