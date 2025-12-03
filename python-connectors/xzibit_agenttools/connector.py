54bf8d973083705a7f5ad72a24a0adfde5992da8
admin <tim.honker@dataiku.com>
2025-12-03 15:36:35 +0000

    Edited file 'python-connectors/xzibit_agenttools/connector.py'

--- python-connectors/xzibit_agenttools/connector.py
+++ python-connectors/xzibit_agenttools/connector.py
@@ -107,28 +107,29 @@ class ConnectorProjects(Connector):
         """TBD"""
         # Data types: https://developer.dataiku.com/latest/api-reference/python/datasets.html#dataiku.core.dataset.Schema
         # Meanings: Text, JSONArrayMeaning, Email, Boolean, DatetimeNoTz, Date, FreeText, LongMeaning
-        return {
-            "columns": [
-                {"meaning": "Text", "name": "Created by user", "type": "string"},
-                {"meaning": "Text", "name": "Last modified by user", "type": "string"},
-                {"meaning": "JSONArrayMeaning", "name": "tags", "type": "string"},
-                {
-                    "meaning": "DatetimeNoTz",
-                    "name": "Last Modified timestamp",
-                    "type": "datetimenotz",
-                },
-                {"meaning": "Text", "name": "projectKey", "type": "string"},
-                {"meaning": "Text", "name": "Agent Name", "type": "string"},
-                {"meaning": "Text", "name": "Agent ID", "type": "string"},
-                {"meaning": "Text", "name": "Active Version", "type": "string"},
-                {"meaning": "Text", "name": "LLM Vendor", "type": "string"},
-                {"meaning": "Text", "name": "LLM Connection Name", "type": "string"},
-                {"meaning": "Text", "name": "LLM Model Name", "type": "string"},
-                {"meaning": "Text", "name": "Agent Type", "type": "string"},
-                {"meaning": "Text", "name": "Agent Version", "type": "string"},
-                {"meaning": "URL", "name": "Agent URL", "type": "string"},
-            ],
-        }
+#         return {
+#             "columns": [
+#                 {"meaning": "Text", "name": "Created by user", "type": "string"},
+#                 {"meaning": "Text", "name": "Last modified by user", "type": "string"},
+#                 {"meaning": "JSONArrayMeaning", "name": "tags", "type": "string"},
+#                 {
+#                     "meaning": "DatetimeNoTz",
+#                     "name": "Last Modified timestamp",
+#                     "type": "datetimenotz",
+#                 },
+#                 {"meaning": "Text", "name": "projectKey", "type": "string"},
+#                 {"meaning": "Text", "name": "Agent Name", "type": "string"},
+#                 {"meaning": "Text", "name": "Agent ID", "type": "string"},
+#                 {"meaning": "Text", "name": "Active Version", "type": "string"},
+#                 {"meaning": "Text", "name": "LLM Vendor", "type": "string"},
+#                 {"meaning": "Text", "name": "LLM Connection Name", "type": "string"},
+#                 {"meaning": "Text", "name": "LLM Model Name", "type": "string"},
+#                 {"meaning": "Text", "name": "Agent Type", "type": "string"},
+#                 {"meaning": "Text", "name": "Agent Version", "type": "string"},
+#                 {"meaning": "URL", "name": "Agent URL", "type": "string"},
+#             ],
+#         }
+        return None
 
     ####################################################################
     # Same for all instances:
