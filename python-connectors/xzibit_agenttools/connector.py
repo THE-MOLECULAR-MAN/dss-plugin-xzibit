9a38b51f180d9692247e5451e4db2f8f50981876
admin <tim.honker@dataiku.com>
2025-12-04 16:44:53 +0000

    Edited file 'python-connectors/xzibit_agenttools/connector.py'

--- python-connectors/xzibit_agenttools/connector.py
+++ python-connectors/xzibit_agenttools/connector.py
@@ -16,7 +16,9 @@ from xzibit.utils import get_dss_base_url, replace_empty_arrays_sets_with_none, 
 def get_agenttool_url(project_key, agenttool_id):
     """TBD"""
     base_url = get_dss_base_url()
-    if agenttool_id is None or agenttool_id is None or project_key is None:
+
+    # at least one is None
+    if any(v is None for v in (agenttool_id, agenttool_id, project_key)):
         return None
     # https://dev-design.se-platform.dataiku-sandbox.io/projects/Data_Dictionary_and_DSS_Instance_datasets_test_project/agent-tools/JfbcCw6
     return f"{base_url}/projects/{project_key}/agent-tools/{agenttool_id}"
