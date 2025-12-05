ea8f662821387d88616150501f4828f2a862f263
THE-MOLECULAR-MAN <>
2025-12-05 00:56:07 -0500

    removed unnecessary print statement

--- python-lib/xzibit/utils.py
+++ python-lib/xzibit/utils.py
@@ -31,7 +31,7 @@ def replace_empty_arrays_sets_with_none(x):
             f"[replace_empty_arrays_sets_with_none] EXCEPTION: {str(type(x))} {str(x)} {e}"
         )
         return x
-    print(f"[replace_empty_arrays_sets_with_none]: {str(type(x))} {str(x)}")
+    # print(f"[replace_empty_arrays_sets_with_none]: {str(type(x))} {str(x)}")
     return x
 
 
