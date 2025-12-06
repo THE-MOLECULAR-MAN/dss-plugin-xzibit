6e13a9ac4adfa0e6283d569d80e57acf2fabe8e6
admin <tim.honker@dataiku.com>
2025-12-06 04:10:13 +0000

    Edited file 'python-connectors/xzibit_plugins/connector.py'

--- python-connectors/xzibit_plugins/connector.py
+++ python-connectors/xzibit_plugins/connector.py
@@ -50,6 +50,8 @@ class ConnectorPlugins(Connector):
         ]
 
         # iterate through each object
+        # list plugins does not take any parameters like object vs list:
+        # https://developer.dataiku.com/latest/api-reference/python/client.html#dataikuapi.DSSClient.list_plugins
         for item_info in self.__client.list_plugins():
             try:
                 if records_limit > 0 and records_generated >= records_limit:
