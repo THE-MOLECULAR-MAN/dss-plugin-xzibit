aced56c1d0d47c7092d92a8f9b0e2a6f91c086e3
admin <tim.honker@dataiku.com>
2025-12-06 04:22:49 +0000

    Edited file 'python-connectors/xzibit_plugins/connector.py'

--- python-connectors/xzibit_plugins/connector.py
+++ python-connectors/xzibit_plugins/connector.py
@@ -66,7 +66,7 @@ class ConnectorPlugins(Connector):
                 # this is so slow!!!!
 
                 #plugin_handle = self.__client.get_plugin(next_row["id"])
-                # .list_usages() adds 2+ hours instead of 1 second.
+                # .list_usages() adds 2+ hours instead of 1 second for entire run
                 #list_of_usages = plugin_handle.list_usages().get_raw()["usages"]
 
 #                 if len(list_of_usages) == 0:
