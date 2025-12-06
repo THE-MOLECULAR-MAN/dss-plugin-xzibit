f30da4c51bf02d5ad652382364b373f3c67e3006
admin <tim.honker@dataiku.com>
2025-12-06 03:01:44 +0000

    Edited file 'python-connectors/xzibit_codeenvs/connector.py'

--- python-connectors/xzibit_codeenvs/connector.py
+++ python-connectors/xzibit_codeenvs/connector.py
@@ -56,7 +56,7 @@ class ConnectorCodeEnvs(Connector):
                 next_row["corePackagesSet"] = settings_raw.get("desc", []).get(
                     "corePackagesSet", []
                 )
-                next_row["path"] = settings.get("path", None)
+                next_row["path"] = settings_raw.get("path", None)
                 
             except Exception as e:
                 print(f"codeenvs - generate_rows EXCEPTION")
