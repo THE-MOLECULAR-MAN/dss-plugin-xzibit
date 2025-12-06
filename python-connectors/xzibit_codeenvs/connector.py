e5e65aae947ba707ae8fe166a73a28ef30733900
admin <tim.honker@dataiku.com>
2025-12-06 01:35:36 +0000

    Edited file 'python-connectors/xzibit_codeenvs/connector.py'

--- python-connectors/xzibit_codeenvs/connector.py
+++ python-connectors/xzibit_codeenvs/connector.py
@@ -34,7 +34,6 @@ class ConnectorCodeEnvs(Connector):
         """TBD"""
         records_generated = 0
         
-        
         for code_env_handle in self.__client.list_code_envs(as_objects=True):
             if records_limit > 0 and records_generated >= records_limit:
                 break
