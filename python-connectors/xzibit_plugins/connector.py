79966251b787cb1ccda4d7a526421bc36fafd4a6
admin <tim.honker@dataiku.com>
2025-12-06 04:22:22 +0000

    Edited file 'python-connectors/xzibit_plugins/connector.py'

--- python-connectors/xzibit_plugins/connector.py
+++ python-connectors/xzibit_plugins/connector.py
@@ -64,6 +64,7 @@ class ConnectorPlugins(Connector):
                 next_row = remove_prefix_from_keys(next_row, "meta.")
                 
                 # this is so slow!!!!
+                # This adds 2+ hours instead of 1 second.
                 #plugin_handle = self.__client.get_plugin(next_row["id"])
                 #list_of_usages = plugin_handle.list_usages().get_raw()["usages"]
 
