214e0ef210d5a0d7e1678ac4bce9dc6a4d36e79f
admin <tim.honker@dataiku.com>
2025-12-06 02:37:42 +0000

    Edited file 'python-connectors/xzibit_codeenvs/connector.py'

--- python-connectors/xzibit_codeenvs/connector.py
+++ python-connectors/xzibit_codeenvs/connector.py
@@ -24,7 +24,7 @@ class ConnectorCodeEnvs(Connector):
         # trailing slash is MANDATORY for Code Envs
         return f"{self.__baseurl}/admin/code-envs/design/{env_lang.lower()}/{env_name}/"
 
-    def generate_rows_faster(
+    def generate_rows(
         self,
         dataset_schema=None,
         dataset_partitioning=None,
@@ -48,83 +48,84 @@ class ConnectorCodeEnvs(Connector):
             yield next_row
 
     
-    def generate_rows(
-        self,
-        dataset_schema=None,
-        dataset_partitioning=None,
-        partition_id=None,
-        records_limit=-1,
-    ):
-        """TBD"""
-        records_generated = 0
-        keys = [
-            "envName",
-            "envLang",
-            "deploymentMode",
-            "pythonInterpreter",
-            "owner",
-        ]
+#     def generate_rows(
+#         self,
+#         dataset_schema=None,
+#         dataset_partitioning=None,
+#         partition_id=None,
+#         records_limit=-1,
+#     ):
+#         """TBD"""
+#         records_generated = 0
+#         keys = [
+#             "envName",
+#             "envLang",
+#             "deploymentMode",
+#             "pythonInterpreter",
+#             "owner",
+#         ]
 
-        # iterate through each object
-        for item_info in self.__client.list_code_envs():
-            if records_limit > 0 and records_generated >= records_limit:
-                break
+#         # iterate through each object
+#         for item_info in self.__client.list_code_envs():
+#             if records_limit > 0 and records_generated >= records_limit:
+#                 break
 
-            next_row = flatten_dict(item_info, include_keys=keys)
+#             next_row = flatten_dict(item_info, include_keys=keys)
 
-            env_lang = next_row["envLang"]
-            env_name = next_row["envName"]
-            try:
-                # next_row["codeenv_url"] = self.get_codeenv_url(env_name, env_lang)
-                next_row["url"] = self.get_url(env_name, env_lang)
-                code_env_handle = self.__client.get_code_env(env_lang, env_name)
-                settings = code_env_handle.get_settings().get_raw()
-                next_row["corePackagesSet"] = settings.get("desc", []).get(
-                    "corePackagesSet", []
-                )
-                next_row["path"] = settings.get("path", None)
+#             env_lang = next_row["envLang"]
+#             env_name = next_row["envName"]
+#             try:
+#                 # next_row["codeenv_url"] = self.get_codeenv_url(env_name, env_lang)
+#                 next_row["url"] = self.get_url(env_name, env_lang)
+#                 code_env_handle = self.__client.get_code_env(env_lang, env_name)
+#                 settings = code_env_handle.get_settings().get_raw()
+#                 next_row["corePackagesSet"] = settings.get("desc", []).get(
+#                     "corePackagesSet", []
+#                 )
+#                 next_row["path"] = settings.get("path", None)
 
-                #                 next_row["disk_size_megabytes"] = get_path_size_megabytes(
-                #                     next_row["path"]
-                #                 )
+#                 #                 next_row["disk_size_megabytes"] = get_path_size_megabytes(
+#                 #                     next_row["path"]
+#                 #                 )
 
-                list_of_usages = code_env_handle.list_usages()
+#                 list_of_usages = code_env_handle.list_usages()
 
-                if len(list_of_usages) == 0:
-                    next_row["usages"] = []
-                else:
-                    next_row["usages"] = list(
-                        get_values_for_key(list_of_usages, "projectKey")
-                    )
+#                 if len(list_of_usages) == 0:
+#                     next_row["usages"] = []
+#                 else:
+#                     next_row["usages"] = list(
+#                         get_values_for_key(list_of_usages, "projectKey")
+#                     )
 
-            except Exception as e:
-                print(
-                    f"[codeenvs-generate_rows] [UNEXPECTED EXCEPTION] {e} on {env_name}"
-                )
+#             except Exception as e:
+#                 print(
+#                     f"[codeenvs-generate_rows] [UNEXPECTED EXCEPTION] {e} on {env_name}"
+#                 )
 
-            finally:
-                # return a single row
-                records_generated += 1
-                yield next_row
+#             finally:
+#                 # return a single row
+#                 records_generated += 1
+#                 yield next_row
 
     def get_read_schema(self):
         """TBD"""
-        return {
-            "columns": [
-                {"meaning": "Text", "name": "envName", "type": "string"},
-                {"meaning": "Text", "name": "envLang", "type": "string"},
-                {"meaning": "Text", "name": "deploymentMode", "type": "string"},
-                {"meaning": "Text", "name": "owner", "type": "string"},
-                {"meaning": "Text", "name": "pythonInterpreter", "type": "string"},
-                {"meaning": "Text", "name": "corePackagesSet", "type": "string"},
-                {"meaning": "Text", "name": "path", "type": "string"},
-                #              {'meaning': 'DoubleMeaning',
-                #               'name': 'disk_size_megabytes',
-                #               'type': 'double'},
-                {"meaning": "JSONArrayMeaning", "name": "usages", "type": "string"},
-                {"meaning": "URL", "name": "url", "type": "string"},
-            ]
-        }
+        return None
+#         return {
+#             "columns": [
+#                 {"meaning": "Text", "name": "envName", "type": "string"},
+#                 {"meaning": "Text", "name": "envLang", "type": "string"},
+#                 {"meaning": "Text", "name": "deploymentMode", "type": "string"},
+#                 {"meaning": "Text", "name": "owner", "type": "string"},
+#                 {"meaning": "Text", "name": "pythonInterpreter", "type": "string"},
+#                 {"meaning": "Text", "name": "corePackagesSet", "type": "string"},
+#                 {"meaning": "Text", "name": "path", "type": "string"},
+#                 #              {'meaning': 'DoubleMeaning',
+#                 #               'name': 'disk_size_megabytes',
+#                 #               'type': 'double'},
+#                 {"meaning": "JSONArrayMeaning", "name": "usages", "type": "string"},
+#                 {"meaning": "URL", "name": "url", "type": "string"},
+#             ]
+#         }
 
     ####################################################################
     # Intentionally not implemented, not needed for this type
