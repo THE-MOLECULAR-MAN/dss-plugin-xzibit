5636ae63bd106d727477e798f3642762ada2e406
admin <tim.honker@dataiku.com>
2025-12-06 02:14:16 +0000

    Edited file 'python-connectors/xzibit_codeenvs/connector.py'

--- python-connectors/xzibit_codeenvs/connector.py
+++ python-connectors/xzibit_codeenvs/connector.py
@@ -37,7 +37,7 @@ class ConnectorCodeEnvs(Connector):
         for code_env_handle in self.__client.list_code_envs(as_objects=True):
             if records_limit > 0 and records_generated >= records_limit:
                 break
-            
+            settings = code_env_handle.get_settings()
 
     
     def generate_rows(
