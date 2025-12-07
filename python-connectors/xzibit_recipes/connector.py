07fc49dbe74583227847e68baf63371511e00e3e
demo <tim.honker@dataiku.com>
2025-12-07 02:47:32 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -3,7 +3,7 @@
 ####################################################################
 from dataiku import api_client
 from dataiku.connector import Connector
-from xzibit.utils import get_dss_base_url
+from xzibit.utils import get_dss_base_url, pp
 
 
 class ConnectorRecipes(Connector):
