84aaea9f8d11bb10e5778ea3dbbd5cbb09d132ad
demo <tim.honker@dataiku.com>
2025-12-07 04:12:45 +0000

    Edited file 'python-connectors/xzibit_agents/connector.py'

--- python-connectors/xzibit_agents/connector.py
+++ python-connectors/xzibit_agents/connector.py
@@ -89,8 +89,9 @@ class ConnectorProjects(Connector):
                         agent = project.get_agent(agent_item.id)
                         settings = agent.get_settings()
                         raw_settings = settings.get_raw()
+                        print("FULL_AGENT_SETTINGS_START")
                         pp(raw_settings)
-
+                        print("FULL_AGENT_SETTINGS_END")
                         # We typically want the LLM used by the *Active* version of the agent
                         next_row["active_agent_version"] = settings.active_version
                         active_version_id = settings.active_version
