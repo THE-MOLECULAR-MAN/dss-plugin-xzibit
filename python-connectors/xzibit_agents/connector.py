c32fe1cacebc5f5c0f50cf2c48dc373e2e1af49b
demo <tim.honker@dataiku.com>
2025-12-07 15:39:47 +0000

    Edited file 'python-connectors/xzibit_agents/connector.py'

--- python-connectors/xzibit_agents/connector.py
+++ python-connectors/xzibit_agents/connector.py
@@ -188,8 +188,7 @@ class ConnectorProjects(Connector):
                             ),
                         }
                         print(f"[agents-generate_rows] about to run recursive_key_search")
-                        #LLMs = recursive_key_search(raw_settings, 'llmId')
-                        #print(f"[agents-generate_rows] 20")
+
                         next_row['LLMs_used'] = "uninitialized"
                         next_row['LLMs_used'] = recursive_search_all(version_settings.get_raw())
                         print(f"[agents-generate_rows] finished without exception")
