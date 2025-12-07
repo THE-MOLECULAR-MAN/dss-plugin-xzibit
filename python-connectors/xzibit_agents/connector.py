01ecefea728f4d12b10951e12de04a357876ac09
demo <tim.honker@dataiku.com>
2025-12-07 15:05:21 +0000

    Edited file 'python-connectors/xzibit_agents/connector.py'

--- python-connectors/xzibit_agents/connector.py
+++ python-connectors/xzibit_agents/connector.py
@@ -11,7 +11,7 @@ from dataiku import api_client
 from dataiku.connector import Connector
 
 # from xzibit.utils import *
-from xzibit.utils import get_dss_base_url, pp
+from xzibit.utils import get_dss_base_url, recursive_key_search, pp
 
 
 def parse_llm_id(llm_string: str):
