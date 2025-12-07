c19be804cb31b32399c77149f0b5ca984d68c27f
demo <tim.honker@dataiku.com>
2025-12-07 16:12:32 +0000

    Edited file 'python-connectors/xzibit_agents/connector.py'

--- python-connectors/xzibit_agents/connector.py
+++ python-connectors/xzibit_agents/connector.py
@@ -145,8 +145,8 @@ class ConnectorProjects(Connector):
                                 agent_item.id, project_key, agent_version
                             ),
                         }
-                        # next_row['LLMs_used'] = recursive_search_all(version_settings.get_raw(), 'llmId') # works but only returns one
-                        next_row['LLMs_used'] = recursive_search_all(raw_settings, 'llmId') # may return multiple
+                        next_row['LLMs_used'] = recursive_search_all(version_settings.get_raw(), 'llmId') # works but only returns one
+                        # next_row['LLMs_used'] = recursive_search_all(raw_settings, 'llmId') # may return multiple
                     except (AttributeError, KeyError, TypeError, ValueError) as e_agent:
                         print(
                             f"[agents-generate_rows] [EXPECTED EXCEPTION] {e_agent} - Project Key: {project_key}, Agent Name: {agent_item.name}"
