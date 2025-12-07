0186fbe6724c371080f8d864b6bb987e17363005
demo <tim.honker@dataiku.com>
2025-12-07 15:41:53 +0000

    Edited file 'python-connectors/xzibit_agents/connector.py'

--- python-connectors/xzibit_agents/connector.py
+++ python-connectors/xzibit_agents/connector.py
@@ -188,7 +188,7 @@ class ConnectorProjects(Connector):
                         }
                         print(f"[agents-generate_rows] about to run recursive_key_search")
                         next_row['LLMs_used'] = "uninitialized"
-                        next_row['LLMs_used'] = recursive_search_all(version_settings.get_raw())
+                        next_row['LLMs_used'] = recursive_search_all(version_settings.get_raw(), 'llmId')
                         print(f"[agents-generate_rows] finished without exception")
                     except (AttributeError, KeyError, TypeError, ValueError) as e_agent:
                         print(
