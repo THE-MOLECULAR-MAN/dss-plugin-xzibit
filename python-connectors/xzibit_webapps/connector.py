9625513f5420ba452ce4ea431343183988e0c380
demo <tim.honker@dataiku.com>
2025-12-07 16:37:33 +0000

    Edited file 'python-connectors/xzibit_webapps/connector.py'

--- python-connectors/xzibit_webapps/connector.py
+++ python-connectors/xzibit_webapps/connector.py
@@ -84,7 +84,7 @@ class ConnectorProjects(Connector):
                             "projectKey": project_key,
                             "webapp_name": webapp.get("name", None),
                             "webapp_id": webapp.get("id", None),
-                            "webapp_type": webapp.get("type"),
+                            "webapp_type": webapp.get("type", None),
                             "created_by_user": webapp.get("createdBy", {}).get("login"),
                             "backend_running": webapp.get("backendRunning", None),
                             "url": self.get_url(
