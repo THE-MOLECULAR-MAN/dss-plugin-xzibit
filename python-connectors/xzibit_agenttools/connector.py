bdce21f433e509012144ab85b014a91e41c69b0a
demo <tim.honker@dataiku.com>
2025-12-08 03:39:39 +0000

    Edited file 'python-connectors/xzibit_agenttools/connector.py'

--- python-connectors/xzibit_agenttools/connector.py
+++ python-connectors/xzibit_agenttools/connector.py
@@ -12,7 +12,7 @@ from dataiku import api_client
 from dataiku.connector import Connector
 
 # from xzibit.utils import *
-from xzibit.utils import get_dss_base_url, replace_empty_arrays_sets_with_none, pp
+from xzibit.utils import get_dss_base_url, replace_empty_arrays_sets_with_none, pp, jd
 
 
 class ConnectorProjects(Connector):
