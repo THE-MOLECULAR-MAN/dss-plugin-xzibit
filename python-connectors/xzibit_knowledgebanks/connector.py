270bdd35986bbcc10d3f7e5449f76c0cd0c2450c
demo <tim.honker@dataiku.com>
2025-12-16 17:09:15 +0000

    Edited file 'python-connectors/xzibit_knowledgebanks/connector.py'

--- python-connectors/xzibit_knowledgebanks/connector.py
+++ python-connectors/xzibit_knowledgebanks/connector.py
@@ -65,7 +65,7 @@ class ConnectorProjects(Connector):
                         # fetch settings as dict
                         settings_raw = kb.get_settings().get_raw()
                         
-                        # display debug info during development, ex: adding features:
+                        # display debug info during development, ex: adding new columns:
                         # pp(settings_raw)
                         
                         # add features that are unique to this object type
