2bed8bfd63bcc5fd5b906c2901d006afd8da78c8
demo <tim.honker@dataiku.com>
2025-12-16 17:31:55 +0000

    Edited file 'python-connectors/xzibit_retrievalaugmentedllms/connector.py'

--- python-connectors/xzibit_retrievalaugmentedllms/connector.py
+++ python-connectors/xzibit_retrievalaugmentedllms/connector.py
@@ -5,7 +5,7 @@
 ####################################################################
 from dataiku import api_client
 from dataiku.connector import Connector
-from xzibit.utils import remove_prefix_from_keys, flatten_dict, get_dss_base_url, pp
+from xzibit.utils import remove_prefix_from_keys, flatten_dict, get_dss_base_url, jd
 
 ####################################################################
 # Unique imports for this Class
