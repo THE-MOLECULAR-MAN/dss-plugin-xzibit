0fa10702e365ea2e30ddc006958746f21cfdd75d
demo <tim.honker@dataiku.com>
2025-12-16 21:05:28 +0000

    Edited file 'python-connectors/xzibit_llmusage/connector.py'

--- python-connectors/xzibit_llmusage/connector.py
+++ python-connectors/xzibit_llmusage/connector.py
@@ -92,7 +92,6 @@ class ConnectorProjects(Connector):
                         data = handle.get_settings().get_raw()
                         pp(data)
                         next_row['dss_object_name'] = data.get('name', None)
-                        # next_row['llmId'] =  data.get('params',{}).get('llmId', None)
                         next_row['llmId'] =  data.get('embeddingLLMId', None)
                     except Exception as e:
                         print(f"[EXCEPTION] generate rows - llm Usage - agent tool: {e}")
