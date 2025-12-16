227f9bae51bd2f989040c1076568a4a0542dfc6f
demo <tim.honker@dataiku.com>
2025-12-16 21:06:45 +0000

    Edited file 'python-connectors/xzibit_llmusage/connector.py'

--- python-connectors/xzibit_llmusage/connector.py
+++ python-connectors/xzibit_llmusage/connector.py
@@ -92,7 +92,8 @@ class ConnectorProjects(Connector):
                         data = handle.get_settings().get_raw()
                         pp(data)
                         next_row['dss_object_name'] = data.get('name', None)
-                        next_row['llmId'] =  data.get('embeddingLLMId', None)
+                        # next_row['llmId'] =  data.get('embeddingLLMId', None)
+                        next_row['llmId'] =  'hardcoded'
                     except Exception as e:
                         print(f"[EXCEPTION] generate rows - llm Usage - agent tool: {e}")
                     finally:
