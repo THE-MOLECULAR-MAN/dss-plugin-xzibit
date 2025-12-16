0a74ae57ec9502a805de7f2506f468e16b83488f
demo <tim.honker@dataiku.com>
2025-12-16 19:52:36 +0000

    Edited file 'python-connectors/xzibit_llmusage/connector.py'

--- python-connectors/xzibit_llmusage/connector.py
+++ python-connectors/xzibit_llmusage/connector.py
@@ -1,128 +1,156 @@
-# This file is the actual code for the custom Python dataset xzibit_llmusage
+"""TBD"""
 
-# import the base class for the custom dataset
-from six.moves import xrange
+####################################################################
+# Same imports for all dataset Classes
+####################################################################
+from dataiku import api_client
 from dataiku.connector import Connector
+from xzibit.utils import remove_prefix_from_keys, flatten_dict, get_dss_base_url, pp
 
-"""
-A custom Python dataset is a subclass of Connector.
+####################################################################
+# Unique imports for this Class
+####################################################################
+from datetime import datetime
 
-The parameters it expects and some flags to control its handling by DSS are
-specified in the connector.json file.
 
-Note: the name of the class itself is not relevant.
-"""
-class MyConnector(Connector):
+class ConnectorProjects(Connector):
+    """TBD"""
 
+    ####################################################################
+    # Code that has to be customized for this specific class
     def __init__(self, config, plugin_config):
-        """
-        The configuration parameters set up by the user in the settings tab of the
-        dataset are passed as a json object 'config' to the constructor.
-        The static configuration parameters set up by the developer in the optional
-        file settings.json at the root of the plugin directory are passed as a json
-        object 'plugin_config' to the constructor
-        """
-        Connector.__init__(self, config, plugin_config)  # pass the parameters to the base class
+        Connector.__init__(self, config, plugin_config)
+        self.__client = api_client()
+        self.__baseurl = get_dss_base_url()
 
-        # perform some more initialization
-        self.theparam1 = self.config.get("parameter1", "defaultValue")
+    def get_url(self, project_key, kb_id):
+        """Create a URL to the object in question in this specific DSS instance.
+        Return None if any of the inputs are None."""
+        # at least one is None, return None
+        if any(v is None for v in (self.__baseurl, project_key, kb_id)):
+            return None
+        return f"{self.__baseurl}/projects/{project_key}/knowledge-bank/{kb_id}/settings/"
+
+    def generate_rows(
+        self,
+        dataset_schema=None,
+        dataset_partitioning=None,
+        partition_id=None,
+        records_limit=-1,
+    ):
+        """TBD"""
+        records_generated = 0
+        
+        # iterate through projects
+        for project_key in self.__client.list_project_keys():
+            try:
+                project = self.__client.get_project(project_key)
+
+                # exit early if exceeded the number of records requested
+                if records_limit > 0 and records_generated >= records_limit:
+                    return
+
+                # iterate through Knowledge Banks in this project as objects
+                # intentionally not using 'list' since that can be slower for some object types
+                for kb in project.list_knowledge_banks(as_type='objects'):
+                    try:
+
+                        # exit early if exceeded the number of records requested
+                        if records_limit > 0 and records_generated >= records_limit:
+                            return
+
+                        # initializing first column in case their is an exception, the yield will still work
+                        next_row = {"projectKey": project_key}
+                        
+                        # fetch settings as dict
+                        settings_raw = kb.get_settings().get_raw()
+                        
+                        # display debug info during development
+                        # for example, finding key names when adding new columns to dataset
+                        # pp(settings_raw)
+                        
+                        # add features that are unique to this object type
+                        next_row["kb_id"] = settings_raw.get('id', None)
+                        next_row["kb_name"] = settings_raw.get('name', None)
+                        next_row["kb_embeddingLLMId"] = settings_raw.get('embeddingLLMId', None)
+                        next_row["retrieverType"] = settings_raw.get('retrieverType', None)
+                        next_row["kb_vectorStoreType"] = settings_raw.get('vectorStoreType', None)
+                        next_row["envSelection"] = settings_raw.get('envSelection', None)
+                        next_row["managedFolderId"] = settings_raw.get("managedFolderId", None)
+                        next_row["multimodalColumn"] = settings_raw.get("multimodalColumn", None)
+                        next_row["rebuildBehavior"] = settings_raw.get("rebuildBehavior", None)
+
+                        # URL is fetched using class method that specifically implements this DSS object type:
+                        next_row["url"] = self.get_url(project_key, next_row["kb_id"])
+                        
+                        # add features that are almost always the same for different DSS object types
+                        next_row["created_timestamp"] = datetime.fromtimestamp(settings_raw.get("creationTag",{}).get("lastModifiedOn", 0) // 1000)
+                        next_row["last_modified_user"] = settings_raw.get("versionTag",{}).get("lastModifiedBy", {}).get("login", None)
+                        next_row["last_modified_timestamp"] = datetime.fromtimestamp(settings_raw.get("versionTag",{}).get("lastModifiedOn", 0) // 1000)
+                        next_row["tags"] = settings_raw.get("tags", None)
+                        next_row["created_by_user"] = settings_raw.get("creationTag",{}).get("lastModifiedBy", {}).get("login", None)                        
+                        
+                    except Exception as e:
+                        print(
+                            f"[generate_rows] [UNEXPECTED EXCEPTION] {e} with object"
+                        )
+                    finally:
+                        # return something, even if an exception occurred.
+                        records_generated += 1
+                        yield next_row                       
+
+            except Exception as e:
+                print(
+                    f"[generate_rows] [UNEXPECTED PROJECT EXCEPTION] {e} with project {project_key}"
+                )
+    
 
     def get_read_schema(self):
-        """
-        Returns the schema that this connector generates when returning rows.
+        """Returns the read schema for TBD"""
+        # Data types: https://developer.dataiku.com/latest/api-reference/python/datasets.html#dataiku.core.dataset.Schema
+        # Meanings: Text, JSONArrayMeaning, Email, Boolean, DatetimeNoTz, Date, FreeText, LongMeaning
+        return {'columns': [{'meaning': 'Text', 'name': 'projectKey', 'type': 'string'},
+             {'meaning': 'Text', 'name': 'kb_id', 'type': 'string'},
+             {'meaning': 'Text', 'name': 'kb_name', 'type': 'string'},
+             {'meaning': 'Text', 'name': 'kb_embeddingLLMId', 'type': 'string'},
+             {'meaning': 'Text', 'name': 'retrieverType', 'type': 'string'},
+             {'meaning': 'Text',
+              'name': 'kb_vectorStoreType',
+              'type': 'string'},
+             {'meaning': 'JSONObjectMeaning',
+              'name': 'envSelection',
+              'type': 'string'},
+             {'meaning': 'DatetimeNoTz',
+              'name': 'created_timestamp',
+              'type': 'string'},
+             {'meaning': 'Text',
+              'name': 'last_modified_user',
+              'type': 'string'},
+             {'meaning': 'DatetimeNoTz',
+              'name': 'last_modified_timestamp',
+              'type': 'string'},
+             {'meaning': 'JSONArrayMeaning', 'name': 'tags', 'type': 'string'},
+             {'meaning': 'Text', 'name': 'created_by_user', 'type': 'string'},
+             {'meaning': 'Text', 'name': 'managedFolderId', 'type': 'string'},
+             {'meaning': 'Text', 'name': 'multimodalColumn', 'type': 'string'},
+             {'meaning': 'Text', 'name': 'rebuildBehavior', 'type': 'string'},
+             {'meaning': 'URL', 'name': 'url', 'type': 'string'}]}
 
-        The returned schema may be None if the schema is not known in advance.
-        In that case, the dataset schema will be infered from the first rows.
-
-        If you do provide a schema here, all columns defined in the schema
-        will always be present in the output (with None value),
-        even if you don't provide a value in generate_rows
-
-        The schema must be a dict, with a single key: "columns", containing an array of
-        {'name':name, 'type' : type}.
-
-        Example:
-            return {"columns" : [ {"name": "col1", "type" : "string"}, {"name" :"col2", "type" : "float"}]}
-
-        Supported types are: string, int, bigint, float, double, date, boolean
-        """
-
-        # In this example, we don't specify a schema here, so DSS will infer the schema
-        # from the columns actually returned by the generate_rows method
+    ####################################################################
+    # Intentionally not implemented, not needed for this type
+    ####################################################################
+    def get_records_count(self, partitioning=None, partition_id=None):
+        """This never runs for anything that I can find."""
         return None
 
-    def generate_rows(self, dataset_schema=None, dataset_partitioning=None,
-                            partition_id=None, records_limit = -1):
-        """
-        The main reading method.
-
-        Returns a generator over the rows of the dataset (or partition)
-        Each yielded row must be a dictionary, indexed by column name.
-
-        The dataset schema and partitioning are given for information purpose.
-        """
-        for i in xrange(1,10):
-            yield { "first_col" : str(i), "my_string" : "Yes" }
-
-
-    def get_writer(self, dataset_schema=None, dataset_partitioning=None,
-                         partition_id=None, write_mode="OVERWRITE"):
-        """
-        Returns a writer object to write in the dataset (or in a partition).
-
-        The dataset_schema given here will match the the rows given to the writer below.
-
-        write_mode can either be OVERWRITE or APPEND.
-        It will not be APPEND unless the plugin explicitly supports append mode. See flag supportAppend in connector.json.
-        If applicable, the write_mode should be handled in the plugin code.
-
-        Note: the writer is responsible for clearing the partition, if relevant.
-        """
-        raise NotImplementedError
-
-
     def get_partitioning(self):
-        """
-        Return the partitioning schema that the connector defines.
-        """
+        """TBD"""
         raise NotImplementedError
-
 
     def list_partitions(self, partitioning):
-        """Return the list of partitions for the partitioning scheme
-        passed as parameter"""
+        """TBD"""
         return []
 
-
     def partition_exists(self, partitioning, partition_id):
-        """Return whether the partition passed as parameter exists
-
-        Implementation is only required if the corresponding flag is set to True
-        in the connector definition
-        """
+        """TBD"""
         raise NotImplementedError
-
-
-    def get_records_count(self, partitioning=None, partition_id=None):
-        """
-        Returns the count of records for the dataset (or a partition).
-
-        Implementation is only required if the corresponding flag is set to True
-        in the connector definition
-        """
-        raise NotImplementedError
-
-
-class CustomDatasetWriter(object):
-    def __init__(self):
-        pass
-
-    def write_row(self, row):
-        """
-        Row is a tuple with N + 1 elements matching the schema passed to get_writer.
-        The last element is a dict of columns not found in the schema
-        """
-        raise NotImplementedError
-
-    def close(self):
-        pass
