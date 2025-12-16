53519f3f88b16a192c0945a8a3239cc7f11b31b9
demo <tim.honker@dataiku.com>
2025-12-16 16:39:37 +0000

    Edited file 'python-connectors/xzibit_knowledgebanks/connector.py'

--- python-connectors/xzibit_knowledgebanks/connector.py
+++ python-connectors/xzibit_knowledgebanks/connector.py
@@ -55,10 +55,7 @@ class ConnectorProjects(Connector):
                         next_row["kb_name"] = webapp.get("name", None)
                         next_row["kb_id"] = webapp.get("id", None)
                    
-                        next_row["url"] = self.get_url(
-                                project_key,
-                                webapp.get("id", "")
-                            )
+                        next_row["url"] = self.get_url(project_key, webapp.get("id", ""))
 
                         next_row["created_timestamp"] = datetime.fromtimestamp(webapp.get("createdOn", 0) // 1000)
                         next_row["last_modified_user"] = webapp.get("lastModifiedBy", {}).get("login", None)
