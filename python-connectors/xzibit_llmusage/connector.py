9d5d876ca53cad605d5c8539e843a74c90e5f72c
demo <tim.honker@dataiku.com>
2025-12-16 21:01:43 +0000

    Edited file 'python-connectors/xzibit_llmusage/connector.py'

--- python-connectors/xzibit_llmusage/connector.py
+++ python-connectors/xzibit_llmusage/connector.py
@@ -59,6 +59,8 @@ class ConnectorProjects(Connector):
                     except Exception as e:
                         print(f"[EXCEPTION] generate rows - llm Usage - agents: {e}")
                     finally:
+                        if not next_row['llmId']:
+                            continue
                         records_generated += 1
                         yield next_row
                         
