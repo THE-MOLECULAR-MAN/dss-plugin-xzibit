0b74b02e242072b2f99471d47c3c6bf4f72d9c08
demo <tim.honker@dataiku.com>
2025-12-16 21:21:09 +0000

    Edited file 'python-connectors/xzibit_llmusage/connector.py'

--- python-connectors/xzibit_llmusage/connector.py
+++ python-connectors/xzibit_llmusage/connector.py
@@ -104,7 +104,7 @@ class ConnectorProjects(Connector):
                         next_row['dss_object_name'] = handle.name
                         data = handle.get_settings().get_recipe_raw_definition()
                         # pp(data)
-                        next_row['llmId'] = recursive_search_all(data, 'llmId')
+                        next_row['llmId'] = recursive_search_all(data, 'llmId') or recursive_search_all(data, 'embeddingLLMId')
                     except Exception as e:
                         print(f"[EXCEPTION] generate rows - llm Usage - recipe: {e}")
                     finally:
