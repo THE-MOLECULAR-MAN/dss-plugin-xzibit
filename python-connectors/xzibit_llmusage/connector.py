1035cd118f6db61fdf51cd32c86dacb6f778d36c
demo <tim.honker@dataiku.com>
2025-12-16 20:57:42 +0000

    Edited file 'python-connectors/xzibit_llmusage/connector.py'

--- python-connectors/xzibit_llmusage/connector.py
+++ python-connectors/xzibit_llmusage/connector.py
@@ -69,8 +69,8 @@ class ConnectorProjects(Connector):
                         
                         data = agent_tool_handle.get_settings().get_raw()
                         pp(data)
-                        next_row['dss_object_name'] = data.get('name',None)
-                        # next_row['llmId'] =  data.get('params',{}).get('llmId',None)
+                        next_row['dss_object_name'] = data.get('name', None)
+                        # next_row['llmId'] =  data.get('params',{}).get('llmId', None)
                         next_row['llmId'] =  recursive_search_all(data, "llmId")
                     except Exception as e:
                         print(f"[EXCEPTION] generate rows - llm Usage - agent tool: {e}")
