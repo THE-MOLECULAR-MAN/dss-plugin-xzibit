5ffef77fce72ab8a110e7d519788c75780e7742e
demo <tim.honker@dataiku.com>
2025-12-17 15:24:13 +0000

    Edited file 'python-connectors/xzibit_connections/connector.py'

--- python-connectors/xzibit_connections/connector.py
+++ python-connectors/xzibit_connections/connector.py
@@ -76,7 +76,7 @@ class ConnectorConnections(Connector):
                 print(f"[Connections-generate_row] EXCEPTION {e}")
                 connection_test_result = 'exception'
                 t = next_row.get("type","unknown type")
-                print(f"{t}")
+                print(f"Connection type had exception: {t}")
             finally:
                 next_row["connection_test_status"] = connection_test_result
                 records_generated += 1
