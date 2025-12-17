25805c81a7fb6a6e6763d66fc59477d3f9d2f3b4
demo <tim.honker@dataiku.com>
2025-12-17 16:37:02 +0000

    Edited file 'python-connectors/xzibit_connections/connector.py'

--- python-connectors/xzibit_connections/connector.py
+++ python-connectors/xzibit_connections/connector.py
@@ -78,7 +78,7 @@ class ConnectorConnections(Connector):
                     connection_test_result = 'FAILED'
                     connection_error_msg = connection_test_status.get("connectionError",{}.get("detailedMessage", "Unable to fetch error message")
                     # pp(connection_test_status)
-
+                
             except DataikuException as e:
                 # We catch the wrapper exception and check the message payload
                 if JAVA_NOT_IMPLEMENTED in str(e):
