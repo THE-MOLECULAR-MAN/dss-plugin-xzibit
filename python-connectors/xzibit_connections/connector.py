80e55a4044f6f6b7ad90bf7f23564d81793e4cae
demo <tim.honker@dataiku.com>
2025-12-17 16:39:13 +0000

    Edited file 'python-connectors/xzibit_connections/connector.py'

--- python-connectors/xzibit_connections/connector.py
+++ python-connectors/xzibit_connections/connector.py
@@ -83,7 +83,6 @@ class ConnectorConnections(Connector):
                 # We catch the wrapper exception and check the message payload
                 if JAVA_NOT_IMPLEMENTED in str(e):
                     connection_test_result = "N/A"
-                    connection_error_msg = None
                 else:
                     # If it is a different Dataiku error, re-raise it to avoid silencing legitimate failures
                     raise e
