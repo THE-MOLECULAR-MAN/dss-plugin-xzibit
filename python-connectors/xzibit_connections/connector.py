604386a80e226d909ecc403614f2cf77ab9e7efe
demo <tim.honker@dataiku.com>
2025-12-17 16:19:31 +0000

    Edited file 'python-connectors/xzibit_connections/connector.py'

--- python-connectors/xzibit_connections/connector.py
+++ python-connectors/xzibit_connections/connector.py
@@ -5,7 +5,7 @@
 ####################################################################
 from dataiku import api_client
 from dataiku.connector import Connector
-from xzibit.utils import flatten_dict, get_dss_base_url, pp, JAVA_NOT_IMPLEMENTED
+from xzibit.utils import flatten_dict, get_dss_base_url, JAVA_NOT_IMPLEMENTED, pp
 
 # Novel:
 from dataikuapi.utils import DataikuException
