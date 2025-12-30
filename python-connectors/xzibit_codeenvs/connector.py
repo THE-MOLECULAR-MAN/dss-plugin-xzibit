8c4fd190dee39e3c586e48baeb8f95d5feb5833c
demo <tim.honker@dataiku.com>
2025-12-30 23:35:28 +0000

    Edited file 'python-connectors/xzibit_codeenvs/connector.py'

--- python-connectors/xzibit_codeenvs/connector.py
+++ python-connectors/xzibit_codeenvs/connector.py
@@ -26,6 +26,7 @@ class ConnectorCodeEnvs(Connector):
         
         self.__compute_codeenv_disk_space_usage = self.config.get("compute_codeenv_disk_space_usage", False)
         
+        # self.__include_usages = False # for possible future configuration option
 
     def get_url(self, env_name, env_lang="python"):
         """Create a URL to the DSS object in question in this specific DSS instance.
