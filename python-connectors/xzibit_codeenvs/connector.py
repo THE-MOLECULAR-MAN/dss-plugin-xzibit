8858a9d656f2c57a4604fee48f162fe7429a7b08
demo <tim.honker@dataiku.com>
2025-12-30 23:17:58 +0000

    Edited file 'python-connectors/xzibit_codeenvs/connector.py'

--- python-connectors/xzibit_codeenvs/connector.py
+++ python-connectors/xzibit_codeenvs/connector.py
@@ -67,7 +67,9 @@ class ConnectorCodeEnvs(Connector):
                 )
                 next_row["path"] = settings_raw.get("path", None)
                 
-                if self.__compute_codeenv_disk_space_usage:
+                if self.__compute_codeenv_disk_space_usage and if next_row["path"]:
+                    if next_row["path"]:
+                        
                     x = get_path_size_megabytes()
                 else:
                     next_row["size_in_MB"] = None
