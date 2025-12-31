bc780da4dd76eb2e76261b4ad28068d319b2cfb6
demo <tim.honker@dataiku.com>
2025-12-31 17:16:47 +0000

    Edited file 'python-connectors/xzibit_plugins/connector.py'

--- python-connectors/xzibit_plugins/connector.py
+++ python-connectors/xzibit_plugins/connector.py
@@ -78,9 +78,10 @@ class ConnectorPlugins(Connector):
                     # this is so slow!!!!
                     plugin_handle = self.__client.get_plugin(next_row["id"])
                     
-                    settings_raw = plugin_handle.get_settings().get_raw()
-                    
-                    pp(settings_raw)
+                    if next_row["id"] == "default-samples"
+                        settings_raw = plugin_handle.get_settings().get_raw()
+                        pp(settings_raw)
+                        
                     # .list_usages() adds 2+ hours instead of 1 second for entire run
                     list_of_usages = plugin_handle.list_usages().get_raw().get("usages",[])
                     if len(list_of_usages) == 0:
