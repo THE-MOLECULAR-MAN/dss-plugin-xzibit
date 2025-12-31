df37ad27bcfaadf0c4dd54e96f44bc793a4eb069
demo <tim.honker@dataiku.com>
2025-12-31 17:24:58 +0000

    Edited file 'python-connectors/xzibit_plugins/connector.py'

--- python-connectors/xzibit_plugins/connector.py
+++ python-connectors/xzibit_plugins/connector.py
@@ -138,7 +138,7 @@ class ConnectorPlugins(Connector):
                 },
                 {"name": "isDev", "type": "boolean", "meaning": "Boolean"},
                 {"name": "total_usages", "type": "bigint", "meaning": "LongMeaning"},
-                "is_built_in_plugin"
+                {"name": "is_built_in_plugin", "type": "boolean", "meaning": "Boolean"},
                 {"name": "url", "type": "string", "meaning": "URL"},
             ]
         }
