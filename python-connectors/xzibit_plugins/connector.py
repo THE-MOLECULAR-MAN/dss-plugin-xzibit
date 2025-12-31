f19dcfd87ebce43b55a277fff4e1b99cedaecd22
demo <tim.honker@dataiku.com>
2025-12-31 17:14:18 +0000

    Edited file 'python-connectors/xzibit_plugins/connector.py'

--- python-connectors/xzibit_plugins/connector.py
+++ python-connectors/xzibit_plugins/connector.py
@@ -79,7 +79,8 @@ class ConnectorPlugins(Connector):
                     plugin_handle = self.__client.get_plugin(next_row["id"])
                     
                     settings_raw = plugin_handle.get_settings().get_raw()
-                    jd(settings_raw)
+                    
+                    print(jd(settings_raw))
                     # .list_usages() adds 2+ hours instead of 1 second for entire run
                     list_of_usages = plugin_handle.list_usages().get_raw().get("usages",[])
                     if len(list_of_usages) == 0:
