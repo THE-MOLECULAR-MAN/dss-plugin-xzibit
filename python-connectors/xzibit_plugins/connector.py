56dd3517de32a00e7f85d4be247d31618e60babd
demo <tim.honker@dataiku.com>
2026-01-02 03:19:44 +0000

    Edited file 'python-connectors/xzibit_plugins/connector.py'

--- python-connectors/xzibit_plugins/connector.py
+++ python-connectors/xzibit_plugins/connector.py
@@ -99,6 +99,7 @@ class ConnectorPlugins(Connector):
                     # this is so slow!!!!
 
                     # .list_usages() adds 2+ hours instead of 1 second for entire run
+                    # on DevDesign, average time per plugin was 26 sec
                     list_of_usages = (
                         plugin_handle.list_usages().get_raw().get("usages", [])
                     )
