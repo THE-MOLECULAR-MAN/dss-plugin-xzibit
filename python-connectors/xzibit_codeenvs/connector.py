7e6a57ba6890e6bda1a5a7558fbfa44af60c7f85
demo <tim.honker@dataiku.com>
2026-01-02 02:59:38 +0000

    Edited file 'python-connectors/xzibit_codeenvs/connector.py'

--- python-connectors/xzibit_codeenvs/connector.py
+++ python-connectors/xzibit_codeenvs/connector.py
@@ -98,7 +98,7 @@ class ConnectorCodeEnvs(Connector):
                 if self.__compute_codeenv_usages:
                     # print(f"starting code env list usages for {next_row['code_env_name']}")
                     usages = code_env_handle.list_usages()
-                    # print(f"finished code env list usages")
+                    print(f"finished code env list usages")
                     num_usages = len(usages)
                     if len(usages) == 0:
                         pk_usages = None
