d72c52fe119b57d3bf48ebf5315c38bd972c88b1
demo <tim.honker@dataiku.com>
2026-01-14 16:43:55 +0000

    Edited file 'python-lib/xzibit/utils.py'

--- python-lib/xzibit/utils.py
+++ python-lib/xzibit/utils.py
@@ -248,7 +248,8 @@ def safe_extract_dataset_metadata(dataset_handle, pk, get_column_lineage=False):
             lineage = []
             
             for colname in dataset_metadata["column_names"]:
-                x = dataset_handle.get_column_lineage()
+                iter_col_lineage = dataset_handle.get_column_lineage()
+                new_lineage = {"column_name": colname, "column_lineage": iter_col_lineage}
                 lineage.append(x)
             dataset_metadata["data_lineage"] = lineage
         else:
