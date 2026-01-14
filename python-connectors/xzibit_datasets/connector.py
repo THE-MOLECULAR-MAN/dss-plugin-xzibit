94af6c86ab73ca1aee337a8806263777c2a24f89
demo <tim.honker@dataiku.com>
2026-01-14 16:38:51 +0000

    Edited file 'python-connectors/xzibit_datasets/connector.py'

--- python-connectors/xzibit_datasets/connector.py
+++ python-connectors/xzibit_datasets/connector.py
@@ -22,6 +22,7 @@ class ConnectorDatasets(Connector):
         self.__client = api_client()
         self.__baseurl = get_dss_base_url()
         self.__get_column_lineage = False
+        "get_column_lineage"
 
     def get_url(self, id, project_key):
         """Create a URL to the DSS object in question in this specific DSS instance.
