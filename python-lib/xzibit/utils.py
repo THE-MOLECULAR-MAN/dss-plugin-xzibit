901c8a3216fb251b85b3253aa45e10bf50a346a1
demo <tim.honker@dataiku.com>
2026-01-14 16:28:28 +0000

    Edited file 'python-lib/xzibit/utils.py'

--- python-lib/xzibit/utils.py
+++ python-lib/xzibit/utils.py
@@ -224,9 +224,11 @@ def safe_extract_dataset_metadata(dataset_handle, pk):
         )
         
         if self.__get_column_lineage:
-            dataset_metadata["data_lineage"] = dataset_handle.get_column_lineage()
+            dataset_metadata["data_lineage"] = []
+            for colname in dataset_metadata["column_names"]:
+            dataset_handle.get_column_lineage()
         else:
-            dataset_metadata["data_lineage"] = ""
+                dataset_metadata.append(dataset_metadata["data_lineage"] = "")
 
     except DataikuException as e:
         print(f"safe_extract_dataset_metadata - Dataiku exception {e}")
