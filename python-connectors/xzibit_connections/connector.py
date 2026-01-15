c1caf35e5f77ed49718b0c5e8c3d3e563a20b962
demo <tim.honker@dataiku.com>
2026-01-15 22:55:18 +0000

    Edited file 'python-connectors/xzibit_connections/connector.py'

--- python-connectors/xzibit_connections/connector.py
+++ python-connectors/xzibit_connections/connector.py
@@ -86,14 +86,17 @@ class ConnectorConnections(Connector):
                 next_row["url"] = self.get_url(next_row["name"])
 
                 obj_handle = self.__client.get_connection(next_row["name"])
-                connection_test_dict = obj_handle.test()  # error
-                if connection_test_dict.get("connectionOK", False):
-                    connection_test_result = "PASSED"
+                if self.__test_connection:
+                    connection_test_dict = obj_handle.test()  # error
+                    if connection_test_dict.get("connectionOK", False):
+                        connection_test_result = "PASSED"
+                    else:
+                        connection_test_result = "FAILED"
+                        connection_error_msg = connection_test_dict.get(
+                            "connectionError", {}
+                        ).get("detailedMessage", "Unable to fetch error message")
                 else:
-                    connection_test_result = "FAILED"
-                    connection_error_msg = connection_test_dict.get(
-                        "connectionError", {}
-                    ).get("detailedMessage", "Unable to fetch error message")
+                    
 
             except Exception as e:
                 # Not all connection types have a .test() method implemented, by design, like filesystem.
