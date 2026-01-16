7daa3d66678ee698342a04467e8e423330602cd2
demo <tim.honker@dataiku.com>
2026-01-16 14:55:52 +0000

    Edited file 'python-connectors/xzibit_pluginusages/connector.py'

--- python-connectors/xzibit_pluginusages/connector.py
+++ python-connectors/xzibit_pluginusages/connector.py
@@ -67,15 +67,6 @@ class ConnectorPlugins(Connector):
                     return
                 
                 plugin_id = item_info.get("id", "NO_PLUGIN_ID")
-                
-                next_row = flatten_dict(item_info, include_keys=keys)
-                next_row = remove_prefix_from_keys(next_row, "meta.")
-
-                next_row["url"] = self.get_url(plugin_id)
-                next_row["is_built_in_plugin"] = (
-                    plugin_id in DSS_BUILT_IN_PLUGIN_IDS
-                )
-
                 plugin_handle = self.__client.get_plugin(plugin_id)
 
                 settings_raw = plugin_handle.get_settings().get_raw()
