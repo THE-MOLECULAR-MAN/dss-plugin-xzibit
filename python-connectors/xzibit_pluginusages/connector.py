06fefefe775ec9459494133fda516867e03fcbd4
demo <tim.honker@dataiku.com>
2026-01-16 14:55:33 +0000

    Edited file 'python-connectors/xzibit_pluginusages/connector.py'

--- python-connectors/xzibit_pluginusages/connector.py
+++ python-connectors/xzibit_pluginusages/connector.py
@@ -67,7 +67,6 @@ class ConnectorPlugins(Connector):
                     return
                 
                 plugin_id = item_info.get("id", "NO_PLUGIN_ID")
-                print(f"[plugins-generate_rows] Start plugin ID: {plugin_id}")
                 
                 next_row = flatten_dict(item_info, include_keys=keys)
                 next_row = remove_prefix_from_keys(next_row, "meta.")
