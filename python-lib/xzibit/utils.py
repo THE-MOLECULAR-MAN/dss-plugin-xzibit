25c17ff35f4d70880c4a0e2311f76d6fcecd9c6a
demo <tim.honker@dataiku.com>
2026-01-16 02:14:53 +0000

    Edited file 'python-lib/xzibit/utils.py'

--- python-lib/xzibit/utils.py
+++ python-lib/xzibit/utils.py
@@ -267,14 +267,14 @@ def safe_extract_dataset_metadata(dataset_handle, pk, get_column_lineage=False):
         else:
             dataset_metadata["data_lineage"] = None
             
-        if get_data_quality_rules:
-            # doubles the runtime from 5 to 9 sec.
-            #print(f"getting data quality rules...")
-            #dataset_metadata["num_data_quality_rules"] = -1
-            dataset_metadata["num_data_quality_rules"] = len(dataset_handle.get_data_quality_rules().list_rules())
-            #print(f"successfully finished getting data quality rules")
-        else:
-            dataset_metadata["num_data_quality_rules"] = None
+#         if get_data_quality_rules:
+#             # doubles the runtime from 5 to 9 sec.
+#             #print(f"getting data quality rules...")
+#             #dataset_metadata["num_data_quality_rules"] = -1
+#             dataset_metadata["num_data_quality_rules"] = len(dataset_handle.get_data_quality_rules().list_rules())
+#             #print(f"successfully finished getting data quality rules")
+#         else:
+#             dataset_metadata["num_data_quality_rules"] = None
 
     except DataikuException as e:
         print(f"safe_extract_dataset_metadata - Dataiku exception {e}")
