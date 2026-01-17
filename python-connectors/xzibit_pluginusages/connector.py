9e4f4dd6b817c167f7d805ee6cb530691ab3b4a2
demo <tim.honker@dataiku.com>
2026-01-17 03:34:03 +0000

    Edited file 'python-connectors/xzibit_pluginusages/connector.py'

--- python-connectors/xzibit_pluginusages/connector.py
+++ python-connectors/xzibit_pluginusages/connector.py
@@ -75,10 +75,7 @@ class ConnectorPlugins(Connector):
 
     def get_read_schema(self):
         """TBD"""
-        # Data types: https://developer.dataiku.com/latest/api-reference/python/datasets.html#dataiku.core.dataset.Schema
-        # Meanings: Text, JSONArrayMeaning, Email, Boolean, DatetimeNoTz, Date, FreeText, LongMeaning
         return None
-
 
     ####################################################################
     # Intentionally not implemented, not needed for this type
