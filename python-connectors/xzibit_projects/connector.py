43d9463ade2fc798477fe7e0b375e07f2259ac12
demo <tim.honker@dataiku.com>
2026-01-17 02:03:01 +0000

    Edited file 'python-connectors/xzibit_projects/connector.py'

--- python-connectors/xzibit_projects/connector.py
+++ python-connectors/xzibit_projects/connector.py
@@ -70,8 +70,6 @@ class ConnectorProjects(Connector):
 
     def get_read_schema(self):
         """TBD"""
-        # Data types: https://developer.dataiku.com/latest/api-reference/python/datasets.html#dataiku.core.dataset.Schema
-        # Meanings: Text, JSONArrayMeaning, Email, Boolean, DatetimeNoTz, Date, FreeText
         return {
             "columns": [
                 {"name": "projectKey", "type": "string", "meaning": "Text"},
