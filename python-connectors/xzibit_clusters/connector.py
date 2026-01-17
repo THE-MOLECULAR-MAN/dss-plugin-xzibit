88f115db179a64117887371abcd75353344ad18e
demo <tim.honker@dataiku.com>
2026-01-17 03:41:55 +0000

    Edited file 'python-connectors/xzibit_clusters/connector.py'

--- python-connectors/xzibit_clusters/connector.py
+++ python-connectors/xzibit_clusters/connector.py
@@ -56,8 +56,6 @@ class ConnectorClusters(Connector):
 
     def get_read_schema(self):
         """TBD"""
-        # Data types: https://developer.dataiku.com/latest/api-reference/python/datasets.html#dataiku.core.dataset.Schema
-        # Meanings: Text, JSONArrayMeaning, Email, Boolean, DatetimeNoTz, Date, FreeText, LongMeaning
         return {
             "columns": [
                 {"meaning": "Text", "name": "id", "type": "string"},
