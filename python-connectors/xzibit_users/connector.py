327a219fc52bbdd61268ddff26f9e51cc4b88280
demo <tim.honker@dataiku.com>
2026-01-17 02:04:25 +0000

    Edited file 'python-connectors/xzibit_users/connector.py'

--- python-connectors/xzibit_users/connector.py
+++ python-connectors/xzibit_users/connector.py
@@ -63,7 +63,6 @@ class ConnectorUsers(Connector):
                 next_row = flatten_dict(item_info, include_keys=keys)
 
                 item_handle = self.__client.get_user(item_info[unique_id_key_name])
-                # activity_handle = item_handle.get_activity()
                 # TODO: fix this date mess below
                 next_row["last_successful_login"] = parse_user_datetime(
                     str(item_handle.get_activity().last_successful_login)
