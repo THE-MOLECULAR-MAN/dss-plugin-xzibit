8e9be0fef21b31a859c8a77caaf835137092ac6e
demo <tim.honker@dataiku.com>
2026-01-17 02:08:40 +0000

    Edited file 'python-lib/xzibit/utils.py'

--- python-lib/xzibit/utils.py
+++ python-lib/xzibit/utils.py
@@ -135,7 +135,6 @@ def get_dss_url_from_env():
         # Note: You may need to infer the scheme (http vs https)
         # based on your knowledge of the instance setup.
         s = f"https://{ext_host}:{base_port}"
-        print(s)
         return s
     return None
 
