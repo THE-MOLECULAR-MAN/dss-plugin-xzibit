8546b2cd76d6dd0750a896bb48ecd98f8ecb8860
demo <tim.honker@dataiku.com>
2026-01-17 02:00:14 +0000

    Edited file 'python-connectors/xzibit_datasets/connector.py'

--- python-connectors/xzibit_datasets/connector.py
+++ python-connectors/xzibit_datasets/connector.py
@@ -52,7 +52,7 @@ class ConnectorDatasets(Connector):
                     return
                 try:
                     dataset_handle = project_handle.get_dataset(r.id)
-                    next_row = safe_extract_dataset_metadata(dataset_handle, pk) #, self.__get_column_lineage) #, self.__get_data_quality_rules)
+                    next_row = safe_extract_dataset_metadata(dataset_handle, pk)
 
                     next_row["url"] = self.get_url(r.id, pk)
 
