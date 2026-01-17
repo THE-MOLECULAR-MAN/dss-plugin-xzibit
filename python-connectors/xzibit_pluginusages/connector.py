ee3309ec7411c76ec7b0e0071166ff19204d3905
demo <tim.honker@dataiku.com>
2026-01-17 03:58:13 +0000

    Edited file 'python-connectors/xzibit_pluginusages/connector.py'

--- python-connectors/xzibit_pluginusages/connector.py
+++ python-connectors/xzibit_pluginusages/connector.py
@@ -5,9 +5,10 @@
 ####################################################################
 from dataiku import api_client
 from dataiku.connector import Connector
-from xzibit.utils import (
-    get_dss_base_url,
-)
+
+# from xzibit.utils import (
+#     get_dss_base_url,
+# )
 
 
 class ConnectorPlugins(Connector):
