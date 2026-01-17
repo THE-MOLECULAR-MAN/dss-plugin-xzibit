92c6f559c9a5abaa29534213860cbda71584dc87
demo <tim.honker@dataiku.com>
2026-01-17 03:33:24 +0000

    Edited file 'python-connectors/xzibit_plugins/connector.py'

--- python-connectors/xzibit_plugins/connector.py
+++ python-connectors/xzibit_plugins/connector.py
@@ -75,12 +75,6 @@ class ConnectorPlugins(Connector):
             "isDev",
         ]
 
-        # iterate through each object
-        # list plugins does not take any parameters like object vs list:
-        # https://developer.dataiku.com/latest/api-reference/python/client.html#dataikuapi.DSSClient.list_plugins
-        # even in the source code, no parameters:
-        # https://github.com/dataiku/dataiku-api-client-python/blob/master/dataikuapi/dssclient.py#L273
-        # There's not an easy way to speed up the next, very slow line
 
         builtin_plugins: list[str] = [
             "default-samples",
