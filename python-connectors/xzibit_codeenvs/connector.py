cb8c45b8f7b12feb0b64be5aecc28b19bd4578b5
demo <tim.honker@dataiku.com>
2026-01-17 03:44:54 +0000

    Edited file 'python-connectors/xzibit_codeenvs/connector.py'

--- python-connectors/xzibit_codeenvs/connector.py
+++ python-connectors/xzibit_codeenvs/connector.py
@@ -7,7 +7,6 @@ from dataiku import api_client
 from dataiku.connector import Connector
 from xzibit.utils import (
     get_dss_base_url,
-    flatten_dict,
     get_values_for_key,
     get_path_size_megabytes,
     pp,
