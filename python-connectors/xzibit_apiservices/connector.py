633e7cf71e9db20094cd865fa90ac34a492c4999
demo <tim.honker@dataiku.com>
2026-01-20 17:07:39 +0000

    Edited file 'python-connectors/xzibit_apiservices/connector.py'

--- python-connectors/xzibit_apiservices/connector.py
+++ python-connectors/xzibit_apiservices/connector.py
@@ -61,11 +61,13 @@ class ConnectorAPIServices(Connector):
                     next_row["api_service_id"] = obj_id
                     next_row["url"] = self.get_url(obj_id, project_key)
                     next_row["num_versions"] = len(obj_handle.list_packages())
-                    
+
                     
                     obj_settings = obj_handle.get_settings().get_raw()
                     print(" === DEBUG === ")
                     pp(obj_settings)
+                    
+                    next_row["last_modified_timestamp"] = 
 
                 except Exception as e:
                     print(
