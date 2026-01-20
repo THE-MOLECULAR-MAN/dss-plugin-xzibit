adbb0d747fbb0ebeb6ca0d510e4d062e4f38f308
demo <tim.honker@dataiku.com>
2026-01-20 19:42:55 +0000

    Edited file 'python-connectors/xzibit_deployments/connector.py'

--- python-connectors/xzibit_deployments/connector.py
+++ python-connectors/xzibit_deployments/connector.py
@@ -67,13 +67,9 @@ class ConnectorAPIServices(Connector):
                 next_row["project_key"] = obj_settings.get("deployedProjectKey","")
                 next_row["infrastructure_id"] = obj_settings.get("infraId","")
                 next_row["deployment_type"] = obj_settings.get("type","")
-
-                
                 
                 next_row["health"] = deployment_handle.get_status().get_health()
                 next_row["highest_governance_severity"] = str(deployment_handle.get_governance_status().get("maxSeverity",""))
-
-                
 
                 next_row["url"] = self.get_url(next_row["bundle_id"], next_row["project_key"])
 
