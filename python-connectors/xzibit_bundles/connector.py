585d626f2abe42a44b752794fe506aed4cf2ef56
demo <tim.honker@dataiku.com>
2026-01-20 16:24:42 +0000

    Edited file 'python-connectors/xzibit_bundles/connector.py'

--- python-connectors/xzibit_bundles/connector.py
+++ python-connectors/xzibit_bundles/connector.py
@@ -59,12 +59,13 @@ class ConnectorBundles(Connector):
                 try:
                     next_row = {"projectKey": project_key} # safe start in case exception happens.
                     next_row["url"] = self.get_url(project_key)
-                    
+
+                    print("=== DEBUG INFO ===")
                     pp(obj_dict)
                     next_row["bundleId"] = obj_dict.get("bundleId","")
                     next_row["bundle_creator"] = obj_dict.get("requester","")
                     next_row["created"] = obj_dict.get("startTime","")
-                    #print("=== DEBUG INFO ===")
+
                     #print(key)
                     #print(value)
                     #pp({key: value})
