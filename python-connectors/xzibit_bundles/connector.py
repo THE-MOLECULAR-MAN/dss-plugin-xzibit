af9730d10b1511262572b9d87e5571fe3839170f
demo <tim.honker@dataiku.com>
2026-01-20 16:20:16 +0000

    Edited file 'python-connectors/xzibit_bundles/connector.py'

--- python-connectors/xzibit_bundles/connector.py
+++ python-connectors/xzibit_bundles/connector.py
@@ -52,7 +52,6 @@ class ConnectorBundles(Connector):
 
             # .list_exported_bundles() returns a dict, not a list
             obj_list = project_handle.list_exported_bundles().get("bundles",[])
-            # pp(x)
             for obj_dict in obj_list:
                 #print(f"[{self.__object_name}-generate_rows] dict key {key}")
                 if records_limit > 0 and records_generated >= records_limit:
