76e1db975c6bc9876c79158f4b20489bf5103570
demo <tim.honker@dataiku.com>
2026-01-20 19:06:12 +0000

    Edited file 'python-connectors/xzibit_deployments/connector.py'

--- python-connectors/xzibit_deployments/connector.py
+++ python-connectors/xzibit_deployments/connector.py
@@ -64,7 +64,7 @@ class ConnectorAPIServices(Connector):
                 obj_settings = deployment_handle.get_settings().get_raw()
                 pp(obj_settings)
                 next_row["bundle_id"] = obj_settings.get("bundleId","")
-                
+                next_row["project_key"] = obj_settings.get("deployedProjectKey","")
 
                 # next_row["url"] = self.get_url(obj_id, project_key)
 
