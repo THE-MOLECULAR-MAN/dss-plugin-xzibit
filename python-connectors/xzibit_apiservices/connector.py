1879e3c23dbb1bb2fd88d7d5865d6e4e486a9634
demo <tim.honker@dataiku.com>
2026-01-20 16:49:37 +0000

    Edited file 'python-connectors/xzibit_apiservices/connector.py'

--- python-connectors/xzibit_apiservices/connector.py
+++ python-connectors/xzibit_apiservices/connector.py
@@ -49,7 +49,7 @@ class ConnectorAPIServices(Connector):
             project_handle = self.__client.get_project(project_key)
 
             # iterate through each object in the project
-            for obj_dict in project_handle.list_api_services(astype="objects"):
+            for obj_handle in project_handle.list_api_services(astype="objects"):
 
                 if records_limit > 0 and records_generated >= records_limit:
                     return
