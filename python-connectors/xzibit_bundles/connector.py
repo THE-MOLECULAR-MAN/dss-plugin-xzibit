996f0f047d1a80b960fed19506e5a57e8d649527
demo <tim.honker@dataiku.com>
2026-01-20 16:06:41 +0000

    Edited file 'python-connectors/xzibit_bundles/connector.py'

--- python-connectors/xzibit_bundles/connector.py
+++ python-connectors/xzibit_bundles/connector.py
@@ -56,7 +56,7 @@ class ConnectorBundles(Connector):
                     next_row = {"projectKey": project_key} # safe start in case exception happens.
                     next_row["url"] = self.get_url(project_key)
                     print("=== DEBUG INFO ===")
-                    pp(obj_dict)
+                    #pp(obj_dict)
 
                 except Exception as e:
                     print(
