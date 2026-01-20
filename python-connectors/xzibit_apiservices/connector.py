ac56253e91d7e1105c1d4fbc9ae21845fadd6b96
demo <tim.honker@dataiku.com>
2026-01-20 17:09:05 +0000

    Edited file 'python-connectors/xzibit_apiservices/connector.py'

--- python-connectors/xzibit_apiservices/connector.py
+++ python-connectors/xzibit_apiservices/connector.py
@@ -68,7 +68,8 @@ class ConnectorAPIServices(Connector):
                     pp(obj_settings)
                     
                     next_row["last_modified_timestamp"] = obj_settings.get("versionTag",{}).get("lastModifiedOn","")
-                    next_row["last_modified_by_user"]   = obj_settings.get("versionTag",{}).get("lastModifiedBy","")                    
+                    next_row["last_modified_by_user"]   = obj_settings.get("versionTag",{}).get("lastModifiedBy","")                 
+                    next_row["num_endpoints"]   = len(obj_settings.get("endpoints",[]))
 
                 except Exception as e:
                     print(
