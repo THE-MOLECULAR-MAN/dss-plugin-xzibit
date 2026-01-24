c011d6f95e3c77f2814f443189b926df015d4d68
admin <tim.honker@dataiku.com>
2026-01-24 22:00:17 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -30,8 +30,8 @@ def prepare_recipe_has_deprecated_preprocessors(prepare_recipe_handle):
         found_dep = preprocessors_unique.intersection(deprecated_preprocessors)
         if isinstance(preprocessors_unique, set) and len(found_dep) > 0:
             # return f"Deprecated preprocessors found: {found_dep}"
-            return found_dep
-    return {}
+            return list(found_dep)
+    return []
 
 class ConnectorRecipes(Connector):
     """TBD"""
