efb830924cf9a49f2eb06bc6b6cf84b73f234a43
admin <tim.honker@dataiku.com>
2026-01-24 22:04:33 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -27,15 +27,17 @@ def prepare_recipe_has_deprecated_preprocessors(prepare_recipe_handle):
     print(f"[prepare_recipe_has_deprecated_preprocessors] - START")
     deprecated_preprocessors = {'AnonymizerProcessor', 'MemoryEquiJoiner', 'MemoryEquiJoinerFuzzy', 'UseRowAsHeader', 'NearestNeighbourGeoJoiner'}
     preprocessors_unique = get_preprocessors_in_prepare_recipe(prepare_recipe_handle)
+    print(f"[prepare_recipe_has_deprecated_preprocessors] - A")
     if isinstance(preprocessors_unique, set):
+        print(f"[prepare_recipe_has_deprecated_preprocessors] - B")        
         found_dep = preprocessors_unique.intersection(deprecated_preprocessors)
-        print(f"[prepare_recipe_has_deprecated_preprocessors] - A")
+        print(f"[prepare_recipe_has_deprecated_preprocessors] - C")
         if isinstance(preprocessors_unique, set) and len(found_dep) > 0:
-            print(f"[prepare_recipe_has_deprecated_preprocessors] - B")
+            print(f"[prepare_recipe_has_deprecated_preprocessors] - D")
             # return f"Deprecated preprocessors found: {found_dep}"
             return list(found_dep)
-        print(f"[prepare_recipe_has_deprecated_preprocessors] - C")
-    print(f"[prepare_recipe_has_deprecated_preprocessors] - D")
+        print(f"[prepare_recipe_has_deprecated_preprocessors] - E")
+    print(f"[prepare_recipe_has_deprecated_preprocessors] - F")
     return []
 
 class ConnectorRecipes(Connector):
