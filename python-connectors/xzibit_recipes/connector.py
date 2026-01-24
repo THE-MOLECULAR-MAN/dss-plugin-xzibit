7c87ab73530262edf40c23ced93560040c1411a9
admin <tim.honker@dataiku.com>
2026-01-24 21:51:37 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -116,16 +116,19 @@ class ConnectorRecipes(Connector):
                         next_row["output_datasets"] = (
                             recipe_settings_handle.get_flat_output_refs()
                         )
-                        next_row["deprecated_prepare_steps_found"]: "e"
+                        next_row["deprecated_prepare_steps_found"]= "about to run"
                         
                         next_row["deprecated_prepare_steps_found"] = prepare_recipe_has_deprecated_preprocessors(recipe_handle)
+                        next_row["deprecated_prepare_steps_found"]= "ran without exception"
                     except Exception as e:
+                        next_row["deprecated_prepare_steps_found"] = "exception 1"                        
                         # this occurs often on Dev-Design.
                         print(
                             f"[recipes-generate_rows] [EXPECTED EXCEPTION] Exception in Recipe output dataset, project_key: {pk}, recipe_id: {r.id}: {e}"
                         )
                 except Exception as e:
                     # this occurs often on Dev-Design.
+                    next_row["deprecated_prepare_steps_found"] = "exception 2"                        
                     print(
                         f"[recipes-generate_rows] [EXPECTED EXCEPTION] Exception in Recipe input dataset, project_key: {pk}, recipe_id: {r.id}: {e}"
                     )
