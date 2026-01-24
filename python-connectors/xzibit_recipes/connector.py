a17ef7af4aa19cddbeb41a6abd894997ebb2f522
admin <tim.honker@dataiku.com>
2026-01-24 22:03:26 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -28,9 +28,9 @@ def prepare_recipe_has_deprecated_preprocessors(prepare_recipe_handle):
     preprocessors_unique = get_preprocessors_in_prepare_recipe(prepare_recipe_handle)
     if isinstance(preprocessors_unique, set):
         found_dep = preprocessors_unique.intersection(deprecated_preprocessors)
-        print(f"[prepare_recipe_has_deprecated_preprocessors] - 10")
+        print(f"[prepare_recipe_has_deprecated_preprocessors] - A")
         if isinstance(preprocessors_unique, set) and len(found_dep) > 0:
-            print(f"[prepare_recipe_has_deprecated_preprocessors] - 20")
+            print(f"[prepare_recipe_has_deprecated_preprocessors] - B")
             # return f"Deprecated preprocessors found: {found_dep}"
             return list(found_dep)
     return []
