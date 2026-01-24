e191a7b5caa6211fb735e1c5e41a05ae624bb9cf
admin <tim.honker@dataiku.com>
2026-01-24 22:01:12 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -117,7 +117,7 @@ class ConnectorRecipes(Connector):
                         )
                         next_row["deprecated_prepare_steps_found"]= "about to run"
                         
-                        #next_row["deprecated_prepare_steps_found"] = prepare_recipe_has_deprecated_preprocessors(recipe_handle)
+                        next_row["deprecated_prepare_steps_found"] = prepare_recipe_has_deprecated_preprocessors(recipe_handle)
                         #next_row["deprecated_prepare_steps_found"]= "ran without exception"
                     except Exception as e:
                         # this occurs often on Dev-Design.
