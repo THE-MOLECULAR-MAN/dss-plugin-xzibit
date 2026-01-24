e092ff1fbdca842b557536d56680d1b9bbd0c5db
admin <tim.honker@dataiku.com>
2026-01-24 22:11:36 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -28,6 +28,7 @@ def prepare_recipe_has_deprecated_preprocessors(prepare_recipe_handle):
     deprecated_preprocessors = {'AnonymizerProcessor', 'MemoryEquiJoiner', 'MemoryEquiJoinerFuzzy', 'UseRowAsHeader', 'NearestNeighbourGeoJoiner'}
     preprocessors_unique = get_preprocessors_in_prepare_recipe(prepare_recipe_handle)
     print(f"[prepare_recipe_has_deprecated_preprocessors] - ran get_preprocessors_in_prepare_recipe without exception")
+    # getting to here okay
     if isinstance(preprocessors_unique, set):
         print(f"[prepare_recipe_has_deprecated_preprocessors] - preprocessors_unique is a set")        
         found_dep = preprocessors_unique.intersection(deprecated_preprocessors)
