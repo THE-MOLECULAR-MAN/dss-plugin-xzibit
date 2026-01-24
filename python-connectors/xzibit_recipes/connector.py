15132b6fe66f82682554c759d2ad22f31ec9d563
admin <tim.honker@dataiku.com>
2026-01-24 22:26:47 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -31,6 +31,9 @@ def get_preprocessors_in_prepare_recipe(prepare_recipe_handle):
 
         return
     except:
+        if DEBUG:
+            print(f"get_preprocessors_in_prepare_recipe - exit EXCEPTION")
+
         return
     
 def prepare_recipe_has_deprecated_preprocessors(prepare_recipe_handle):
