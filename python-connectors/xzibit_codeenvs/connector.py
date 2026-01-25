42ace4531c423fcaa78eb75917b2acad1ddcc33e
demo <tim.honker@dataiku.com>
2026-01-25 04:25:53 +0000

    Edited file 'python-connectors/xzibit_codeenvs/connector.py'

--- python-connectors/xzibit_codeenvs/connector.py
+++ python-connectors/xzibit_codeenvs/connector.py
@@ -134,12 +134,12 @@ class ConnectorCodeEnvs(Connector):
                     next_row["projectKeys_where_code_env_used"] = []
                     next_row["total_instances_of_code_env"] = -1
 
-                    # print(f"starting code env list usages for {code_env_name}")
+                    print(f"starting code env list usages for {code_env_name}")
                     # next line throws exception on DevDesign:
                     #  jakarta.servlet.ServletException: Handler dispatch failed: java.lang.Error: Unknown tool type: Custom_agent_tool_jira-tools_jira-create-issue-tool, caused by: Error: Unknown tool type: Custom_agent_tool_jira-tools_jira-create-issue-tool
                     # list_usages() does not take any parameters
                     usages = code_env_handle.list_usages()
-                    # print(f"Finished code env list usages for {code_env_name}")
+                    print(f"Finished code env list usages for {code_env_name}")
                     num_usages = len(usages)
                     if len(usages) == 0:
                         pk_usages = None
