f2460ee7eca7250c1fc0d978d9a0a5d5903508ed
demo <tim.honker@dataiku.com>
2026-01-25 04:06:39 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -133,7 +133,7 @@ class ConnectorRecipes(Connector):
                         else:
                             next_row["deprecation_status"] = (
                                 lookup_recipe_deprecation_status(
-                                    next_row["recipe_type"]
+                                    next_row["recipe_type"], 
                                 )
                             )
 
