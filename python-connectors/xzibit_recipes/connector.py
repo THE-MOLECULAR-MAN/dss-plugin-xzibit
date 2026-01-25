caab7914592f1a1941703ff90f8ae5845e471b91
demo <tim.honker@dataiku.com>
2026-01-25 04:01:53 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -134,7 +134,7 @@ class ConnectorRecipes(Connector):
                         )
                         
                         if len(deprecated_preprocessors) > 0:
-                            next_row["deprecation_status"] = "Prepare recipe uses deprecated pre"
+                            next_row["deprecation_status"] = "Prepare recipe uses deprecated preprocessors"
 
                         next_row["deprecation_status"] = (
                             lookup_recipe_deprecation_status(next_row["recipe_type"])
