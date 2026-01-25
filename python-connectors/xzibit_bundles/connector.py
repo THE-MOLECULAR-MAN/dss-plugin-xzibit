b87622583010dbaf5d515cb13c20e3b50d23ec77
demo <tim.honker@dataiku.com>
2026-01-25 00:29:44 +0000

    Edited file 'python-connectors/xzibit_bundles/connector.py'

--- python-connectors/xzibit_bundles/connector.py
+++ python-connectors/xzibit_bundles/connector.py
@@ -5,10 +5,7 @@
 ####################################################################
 from dataiku import api_client
 from dataiku.connector import Connector
-from xzibit.utils import (
-    get_dss_base_url,
-    pp
-)
+from xzibit.utils import get_dss_base_url, pp
 
 
 class ConnectorBundles(Connector):
@@ -49,46 +46,68 @@ class ConnectorBundles(Connector):
             project_handle = self.__client.get_project(project_key)
 
             # .list_exported_bundles() returns a dict, not a list
-            obj_list = project_handle.list_exported_bundles().get("bundles",[])
-            
+            obj_list = project_handle.list_exported_bundles().get("bundles", [])
+
             # iterate through each object in the project
             for obj_dict in obj_list:
 
                 if records_limit > 0 and records_generated >= records_limit:
                     return
                 try:
-                    next_row = {"project_key": project_key} # safe start in case exception happens.
+                    next_row = {
+                        "project_key": project_key
+                    }  # safe start in case exception happens.
                     next_row["url"] = self.get_url(project_key)
 
-                    next_row["bundle_id"]   = obj_dict.get("bundleId","")
-                    next_row["state"]       = obj_dict.get("state","")
-                    next_row["type_badges"] = obj_dict.get("typeBadges",[])                    
-                    
-                    next_row["exported_by_user"]        = obj_dict.get("exportManifest",{}).get("exportUserInfo",{}).get("exportedBy","")
-                    next_row["exported_timestamp"]      = obj_dict.get("exportManifest",{}).get("exportUserInfo",{}).get("exportedOn","")
-                    next_row["release_notes"]           = obj_dict.get("exportManifest",{}).get("exportUserInfo",{}).get("releaseNotes","")
-                    next_row["projectStandardsSkipped"] = obj_dict.get("exportManifest",{}).get("projectStandardsSkipped","")                   
+                    next_row["bundle_id"] = obj_dict.get("bundleId", "")
+                    next_row["state"] = obj_dict.get("state", "")
+                    next_row["type_badges"] = obj_dict.get("typeBadges", [])
 
-                    next_row["generated_with_dss_version"] = obj_dict.get("exportManifest",{}).get("generatedWithDSSVersion","")
-                    next_row["exported_on_design_node_id"] = obj_dict.get("exportManifest",{}).get("designNodeId","")
-                    
-                    next_row["published_timestamp"] = obj_dict.get("publishedBundleState",{}).get("publishedOn","")
-                    next_row["published_by_user"]   = obj_dict.get("publishedBundleState",{}).get("publishedBy","")                    
+                    next_row["exported_by_user"] = (
+                        obj_dict.get("exportManifest", {})
+                        .get("exportUserInfo", {})
+                        .get("exportedBy", "")
+                    )
+                    next_row["exported_timestamp"] = (
+                        obj_dict.get("exportManifest", {})
+                        .get("exportUserInfo", {})
+                        .get("exportedOn", "")
+                    )
+                    next_row["release_notes"] = (
+                        obj_dict.get("exportManifest", {})
+                        .get("exportUserInfo", {})
+                        .get("releaseNotes", "")
+                    )
+                    next_row["projectStandardsSkipped"] = obj_dict.get(
+                        "exportManifest", {}
+                    ).get("projectStandardsSkipped", "")
+
+                    next_row["generated_with_dss_version"] = obj_dict.get(
+                        "exportManifest", {}
+                    ).get("generatedWithDSSVersion", "")
+                    next_row["exported_on_design_node_id"] = obj_dict.get(
+                        "exportManifest", {}
+                    ).get("designNodeId", "")
+
+                    next_row["published_timestamp"] = obj_dict.get(
+                        "publishedBundleState", {}
+                    ).get("publishedOn", "")
+                    next_row["published_by_user"] = obj_dict.get(
+                        "publishedBundleState", {}
+                    ).get("publishedBy", "")
 
                 except Exception as e:
                     print(
                         f"[{self.__object_name}-generate_rows] [UNEXPECTED EXCEPTION] with {self.__object_name} in project {project_key}: {e}"
                     )
-                    
+
                 finally:
                     records_generated += 1
                     yield next_row
 
-
     def get_read_schema(self):
         """TBD"""
         return None
-
 
     ####################################################################
     # Intentionally not implemented, not needed for this type
