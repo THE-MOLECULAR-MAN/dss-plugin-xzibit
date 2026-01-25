77f4fefc83f25baaa456d99c9bbddaa0d2745fd4
demo <tim.honker@dataiku.com>
2026-01-25 03:15:24 +0000

    Edited file 'python-connectors/xzibit_codeenvs/connector.py'

--- python-connectors/xzibit_codeenvs/connector.py
+++ python-connectors/xzibit_codeenvs/connector.py
@@ -98,6 +98,8 @@ class ConnectorCodeEnvs(Connector):
                     python_version_formatted = (
                         python_version_formatted[0] + "." + python_version_formatted[1:]
                     )
+                    
+                    
 
                 # next_row["python_version_support_status"] = lookup_python_support(
                 #     "14", "3.10", self.__df_dss_python
