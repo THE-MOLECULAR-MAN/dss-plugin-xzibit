36e0f566372714ac993f917743d1a3ef7a05c25d
demo <tim.honker@dataiku.com>
2026-01-25 01:31:48 +0000

    Edited file 'python-lib/xzibit/deprecations.py'

--- python-lib/xzibit/deprecations.py
+++ python-lib/xzibit/deprecations.py
@@ -74,5 +74,6 @@ def load_python_support(filepath="./DSS_version_python_support.csv"):
     """TBD"""
     if not filepath:
         return pd.DataFrame()
+    if 
     df = pd.read_csv(filepath, dtype=str)
     return df
