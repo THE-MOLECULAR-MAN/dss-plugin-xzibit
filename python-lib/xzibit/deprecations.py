3fbcebc479cfc005f699bf519f3e44a2b51cb1cf
demo <tim.honker@dataiku.com>
2026-01-25 02:43:31 +0000

    Edited file 'python-lib/xzibit/deprecations.py'

--- python-lib/xzibit/deprecations.py
+++ python-lib/xzibit/deprecations.py
@@ -84,7 +84,7 @@ def load_python_support(filepath="./DSS_version_python_support.csv"):
 df_python_support = load_python_support()
 
 
-def lookup_python_support(dss_version, python_version, df=df_python_support):
+def lookup_python_support(dss_version, python_version, df=load_python_support()):
     """TBD"""
     if df is None:
         return None
