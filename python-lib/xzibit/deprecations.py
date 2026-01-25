2cd5afc70788294032031d7b05280c59a73bee33
demo <tim.honker@dataiku.com>
2026-01-25 02:46:01 +0000

    Edited file 'python-lib/xzibit/deprecations.py'

--- python-lib/xzibit/deprecations.py
+++ python-lib/xzibit/deprecations.py
@@ -92,7 +92,7 @@ def lookup_python_support(dss_version, python_version, df=load_python_support())
     ]
 
     if row.empty:
-        return None
+        return "None"
 
     return row.iloc[0]["Support_Status"]
 
