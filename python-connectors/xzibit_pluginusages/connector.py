7e0de26f8e48a5cc95426ad2b2d4438351058c0f
demo <tim.honker@dataiku.com>
2026-01-25 00:49:43 +0000

    Edited file 'python-connectors/xzibit_pluginusages/connector.py'

--- python-connectors/xzibit_pluginusages/connector.py
+++ python-connectors/xzibit_pluginusages/connector.py
@@ -60,7 +60,7 @@ class ConnectorPlugins(Connector):
                         next_row["object_id"] = usage.object_id
                         next_row["object_type"] = usage.object_type
                         next_row["project_key"] = usage.project_key
-                        next_row["is_dep"]
+                        next_row["is_deprecated_plugin"] = plugin_id in DEPRECATED_PLUGIN_IDS
                         
                         from xzibit.deprecations import DEPRECATED_PLUGIN_IDS, DSS_BUILT_IN_PLUGIN_IDS
 
