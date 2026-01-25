cd95e7b339bc0a0137149ab5eb4b97695c9bb3d4
demo <tim.honker@dataiku.com>
2026-01-25 00:52:33 +0000

    Edited file 'python-lib/xzibit/deprecations.py'

--- python-lib/xzibit/deprecations.py
+++ python-lib/xzibit/deprecations.py
@@ -67,4 +67,5 @@ DSS_BUILT_IN_PLUGIN_IDS = [
     "project-standards",
 ]
 
-SUPPORTED_PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11", "3.12"]
+SUPPORTED_PYTHON_VERSIONS = {
+                             }
