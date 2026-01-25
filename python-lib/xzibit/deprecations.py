124eb9c206fafa5f16b23270cd9ba5ae72228746
demo <tim.honker@dataiku.com>
2026-01-25 01:32:54 +0000

    Edited file 'python-lib/xzibit/deprecations.py'

--- python-lib/xzibit/deprecations.py
+++ python-lib/xzibit/deprecations.py
@@ -77,6 +77,6 @@ def load_python_support(filepath="./DSS_version_python_support.csv"):
         df = pd.read_csv(filepath, dtype=str)
 
     except FileNotFoundError:
-        return pd.DataFrame()
+        df pd.DataFrame()
 
     return df
