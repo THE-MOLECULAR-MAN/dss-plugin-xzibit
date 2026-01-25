c62a4152a486d6bce0ed551e4bd29a6f51d3a6eb
demo <tim.honker@dataiku.com>
2026-01-25 03:56:00 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -133,7 +133,7 @@ class ConnectorRecipes(Connector):
                             prepare_recipe_has_deprecated_preprocessors(recipe_handle)
                         )
                         
-                        next_row["recipe_deprecation_status"] = lookup_recipe_deprecation_status(re)
+                        next_row["recipe_type_deprecation_status"] = lookup_recipe_deprecation_status(re)
 
                     except Exception as e:
                         # this occurs often on Dev-Design.
