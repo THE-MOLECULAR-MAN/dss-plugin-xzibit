e5abb08141154f566c5a6594181b4aad85d950c0
demo <tim.honker@dataiku.com>
2026-01-25 00:22:05 +0000

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -5,7 +5,8 @@
 ####################################################################
 from dataiku import api_client
 from dataiku.connector import Connector
-from xzibit.utils import get_dss_base_url, , pp
+from xzibit.utils import get_dss_base_url, pp
+from xzibit.deprecations import DEPRECATED_PREPROCESSORS
 
 
 def get_unique_types(data_list):
