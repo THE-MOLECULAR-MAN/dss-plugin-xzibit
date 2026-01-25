8ed2f02b3c2c10428f1f14cb7430f7f7426165e1
demo <tim.honker@dataiku.com>
2026-01-25 00:07:08 +0000

    Edited file 'python-lib/xzibit/utils.py'

--- python-lib/xzibit/utils.py
+++ python-lib/xzibit/utils.py
@@ -13,8 +13,13 @@ from pprint import pprint as pp
 from json import dumps as jd
 
 
-deprecated_preprocessors = {'AnonymizerProcessor', 'MemoryEquiJoiner', 'MemoryEquiJoinerFuzzy', 'UseRowAsHeader', 'NearestNeighbourGeoJoiner'}
-
+deprecated_preprocessors = {
+    "AnonymizerProcessor",
+    "MemoryEquiJoiner",
+    "MemoryEquiJoinerFuzzy",
+    "UseRowAsHeader",
+    "NearestNeighbourGeoJoiner",
+}
 
 
 JAVA_NOT_IMPLEMENTED = "com.dataiku.dip.utils.NotImplementedException"
