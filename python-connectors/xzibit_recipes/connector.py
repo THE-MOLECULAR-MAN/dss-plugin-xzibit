a31a7383f6d2f1ce1892a66f0b2ba3951d478003
admin <admin>
2026-01-26 12:42:08 -0500

    Edited file 'python-connectors/xzibit_recipes/connector.py'

--- python-connectors/xzibit_recipes/connector.py
+++ python-connectors/xzibit_recipes/connector.py
@@ -5,7 +5,7 @@
 ####################################################################
 from dataiku import api_client
 from dataiku.connector import Connector
-from xzibit.utils import get_dss_base_url, pp
+from xzibit.utils import get_dss_base_url, pp,get_python_recipe_code_env
 from xzibit.deprecations import (
     DEPRECATED_PREPROCESSORS,
     load_local_csv_as_dataframe,
