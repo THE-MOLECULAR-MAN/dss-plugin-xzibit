a3d46585d243b8d719660e787f6072361db6f7a6
admin <admin>
2026-01-27 13:17:24 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -198,7 +198,7 @@ class ConnectorPythonAnalysis(Connector):
             # For simplicity in this connector, let's return the count of issues found
             return len(data)
         except json.JSONDecodeError:
-            logger.error(f"loaded json for pylint")
+            logger.error(f"_analyze_pylint")
             return -1.0
         except Exception:
             return -1.0
