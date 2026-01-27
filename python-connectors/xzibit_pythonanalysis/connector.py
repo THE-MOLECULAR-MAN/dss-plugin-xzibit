0df7ff81dddeaf15927320333e30053156e68d57
admin <admin>
2026-01-27 13:21:57 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -105,6 +105,7 @@ class ConnectorPythonAnalysis(Connector):
             # detect returns (mins, parsable, text)
             logger.info(f"_analyze_vermin start")
             mins = detect(code, config=self.vermin_config).mins
+            logger.info(f"_analyze_vermin start")
             if mins:
                 # Returns something like "3.8"
                 return str(max(mins))
