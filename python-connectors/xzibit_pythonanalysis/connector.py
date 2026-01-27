b2787b6c710d5283b0aa86abf2726480f93389bb
admin <admin>
2026-01-27 17:46:29 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -293,7 +293,7 @@ class ConnectorPythonAnalysis(Connector):
                                 min_python_version_from_vermin
                             )
 
-                            is_python2 = mpvv[0] == 2
+                            is_python3 = mpvv[0] == 3
 
                             # 2. Radon (Complexity) - WORKING
                             radon_metrics = self._analyze_radon(code)
