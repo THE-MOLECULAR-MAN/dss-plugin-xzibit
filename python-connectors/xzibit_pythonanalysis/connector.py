9a4fcd9d719383b70d082ce6b5acaa2d7b96f16a
admin <admin>
2026-01-27 11:45:27 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -80,6 +80,7 @@ class ConnectorPythonAnalysis(Connector):
             if not code_env_name:
                 return "Code Env Name Not Specified"
             logger.info(f"Fetching handle for code env {code_env_name}")
+            # next line is causing 
             code_env = self.__client.get_code_env(code_env_name)
             logger.info(f"Successfully fetched handle for code env {code_env_name}")
             return (
