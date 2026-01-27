eab7746ea03c5fe709372112eacc11d589c211e7
admin <admin>
2026-01-26 22:05:11 -0500

    Edited file 'python-connectors/xzibit_pluginusages/connector.py'

--- python-connectors/xzibit_pluginusages/connector.py
+++ python-connectors/xzibit_pluginusages/connector.py
@@ -63,7 +63,7 @@ class ConnectorPlugins(Connector):
                         }
 
                         next_row["object_id"] = dataset_handle.id
-                        next_row["subtype"] = dataset_info.type
+                        next_row["subtype"] = dataset_info.get
 
                         dataset_info = dataset_handle.get_info().get_raw()
                         pp(dataset_info)
