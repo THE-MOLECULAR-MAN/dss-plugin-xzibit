a8f9fdc0da173af17e10f4480d0f71dc3164ee0f
admin <admin>
2026-01-27 11:22:49 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -228,6 +228,7 @@ class ConnectorPythonAnalysis(Connector):
                         }
                         try:
                             # get additional Dataiku metadata
+                            print("x")
                             code_env_name = self.get_recipe_code_env_name(name)
                             # python_version = self.get_code_env_python_version(
                             #     code_env_name
