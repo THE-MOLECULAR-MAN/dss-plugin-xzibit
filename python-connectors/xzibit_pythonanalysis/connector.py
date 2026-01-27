4ab0e9d905b323575c8c7603c17d82a1625e2e01
admin <admin>
2026-01-27 11:38:20 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -80,10 +80,13 @@ class ConnectorPythonAnalysis(Connector):
             if not code_env_name:
                 return "Project Default"
             code_env = self.__client.get_code_env(code_env_name)
-            return code_env.get_settings().get_raw().get("desc", {}).get(
-                    "pythonInterpreter", None
-                )
-        
+            return (
+                code_env.get_settings()
+                .get_raw()
+                .get("desc", {})
+                .get("pythonInterpreter", None)
+            )
+
         except Exception:
             logger.warning(
                 f"Could not retrieve Python version for code environment {code_env_name}."
