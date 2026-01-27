2e038c901938357f68525fbb942eb9adba606bcd
admin <admin>
2026-01-27 13:14:18 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -268,7 +268,7 @@ class ConnectorPythonAnalysis(Connector):
 
                             # # 3. Dependencies (AST/Deptry logic)
                             deps = self._analyze_dependencies(code)
-                            # depend
+                            # dependencies_list returns a syntax error 
                             next_row["dependencies_list"] = ",".join(deps)
                             next_row["dependencies_count"] = len(deps)
 
