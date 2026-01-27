341cba663e3654075cfd8cec172d1e2d0e62f1e7
admin <admin>
2026-01-27 09:43:59 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -83,7 +83,7 @@ class ConnectorPythonAnalysis(Connector):
                                 f"Error analyzing {name} in {project_key}: {e}"
                             )
                         finally:
-                            # Re
+                            # Retu
                             records_generated += 1
                             yield next_row
 
