fcd83bf6cf14e6eb207ac83dd36db46bee126357
admin <admin>
2026-01-26 20:59:50 -0500

    Edited file 'python-connectors/xzibit_pluginusages/connector.py'

--- python-connectors/xzibit_pluginusages/connector.py
+++ python-connectors/xzibit_pluginusages/connector.py
@@ -47,6 +47,8 @@ class ConnectorPlugins(Connector):
                 if records_limit > 0 and records_generated >= records_limit:
                     return
 
+                obj_uses_plugin = False
+
                 try:
 
                     next_row = {
