7a33e99d9a842033387fc4a798f4aad97b188aab
admin <admin>
2026-01-26 21:01:22 -0500

    Edited file 'python-connectors/xzibit_pluginusages/connector.py'

--- python-connectors/xzibit_pluginusages/connector.py
+++ python-connectors/xzibit_pluginusages/connector.py
@@ -59,7 +59,7 @@ class ConnectorPlugins(Connector):
 
                         next_row["object_id"] = dataset_handle.id
 
-                    dataset_info = dataset_handle.get_definition()
+                        dataset_info = dataset_handle.get_definition()
 
                 except Exception:
                     print("plugin_usages - Exception occurred")
