5a57b467e32296d5b2c52e4e99ec74bad1eeca0f
admin <admin>
2026-01-27 08:21:59 -0500

    Edited file 'python-connectors/xzibit_codeenvs/connector.py'

--- python-connectors/xzibit_codeenvs/connector.py
+++ python-connectors/xzibit_codeenvs/connector.py
@@ -154,6 +154,7 @@ class ConnectorCodeEnvs(Connector):
 
             except Exception as e:
                 # this is occuring on DevDesign
+                next_row["total_instances_of_code_env"] =
                 print(
                     f"codeenvs - generate_rows EXCEPTION: CodeEnv: {code_env_name} Error message: {e}"
                 )
