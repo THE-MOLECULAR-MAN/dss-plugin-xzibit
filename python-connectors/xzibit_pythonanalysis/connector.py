fc5d6968a0c4556f71a190b6b3bb09f5da074fa9
admin <admin>
2026-01-27 17:39:54 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -100,7 +100,7 @@ class ConnectorPythonAnalysis(Connector):
             logger.info(
                 f"Successfully fetched handle for Python code env {code_env_name}"
             )
-             code_env.get_settings()
+            PY_INTERP code_env.get_settings()
                 .get_raw()
                 .get("desc", {})
                 .get("pythonInterpreter", None)
