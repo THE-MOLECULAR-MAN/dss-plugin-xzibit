de393cddfac34ec9297d0bd64bcfbac1916b6461
admin <admin>
2026-01-27 09:48:45 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -105,11 +105,11 @@ class ConnectorPythonAnalysis(Connector):
                         }
                         try:
                             # get additional Dataiku metadata
-                            code_env_name = 
-                            python_version = 
-                            next_row["code_env_name"] = self.get_recipe_code_env_name(name)
-                            next_row["python_version"] = self.get_code_env_python_version(
-                                code_env_name
+                            next_row["code_env_name"] = self.get_recipe_code_env_name(
+                                name
+                            )
+                            next_row["python_version"] = (
+                                self.get_code_env_python_version(code_env_name)
                             )
 
                             # vermin analysis for minimum Python versions
