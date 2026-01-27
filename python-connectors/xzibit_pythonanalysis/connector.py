74957d6d67019ab0e2514f49027515cf93173bb9
admin <admin>
2026-01-27 09:47:34 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -46,7 +46,7 @@ class ConnectorPythonAnalysis(Connector):
             )
             return ""
         
-    def get_recipe_code_env_name
+    def get_recipe_code_env_name(self, recipe_name: str) -> str:
 
     def generate_rows(
         self,
