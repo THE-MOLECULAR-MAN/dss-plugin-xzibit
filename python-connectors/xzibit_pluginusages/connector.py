72d6ed9e5c97ccfcffae50b034363edc584d9a9e
admin <admin>
2026-01-26 22:08:24 -0500

    Edited file 'python-connectors/xzibit_pluginusages/connector.py'

--- python-connectors/xzibit_pluginusages/connector.py
+++ python-connectors/xzibit_pluginusages/connector.py
@@ -57,7 +57,7 @@ class ConnectorPlugins(Connector):
                     # determine if dataset uses a plugin
                     dataset_info = dataset_handle.get_info().get_raw()
                     dataset_type = dataset_info.get("type", "")
-                    obj_uses_plugin = dataset_type.startswith("Custom")
+                    obj_uses_plugin = 
 
                     if obj_uses_plugin:
                         next_row = {
