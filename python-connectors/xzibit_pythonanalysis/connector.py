c2ec5acf82e0684b538a07a313cf0a0f63d61536
admin <admin>
2026-01-27 09:32:46 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -14,7 +14,7 @@ from datetime import datetime
 
 
 class ConnectorPythonAnalysis(Connector):
-    """A Dataiku DSS v12 connector to list projects and their Python code analysis results."""
+    """A Dataiku DSS v12 connector to provide a DSS Dataset listing all list projects and their Python code analysis results."""
 
     ####################################################################
     # Code that has to be customized for this specific class
