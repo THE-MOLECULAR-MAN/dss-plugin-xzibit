3ef972259bef18274e03892a038a0a43bf2db051
admin <admin>
2026-01-26 20:51:08 -0500

    Edited file 'python-connectors/xzibit_pluginusages/connector.py'

--- python-connectors/xzibit_pluginusages/connector.py
+++ python-connectors/xzibit_pluginusages/connector.py
@@ -48,21 +48,21 @@ class ConnectorPlugins(Connector):
                     return
                 try:
                     dataset_info = dataset_handle.get_definition()
-                    
+
                     next_row = {
-                    "projectKey": pk,
-                    "dataset_id": dataset_handle.id,
-                    "dataset_name": dataset_handle.name,
-                    "dataset_type": dataset_info.get("type", None),
-                    "is_managed": dataset_info.get("managed", None),
-                }
+                        "projectKey": pk,
+                        "dataset_id": dataset_handle.id,
+                        "dataset_name": dataset_handle.name,
+                        "dataset_type": dataset_info.get("type", None),
+                        "is_managed": dataset_info.get("managed", None),
+                    }
 
                 except Exception as e:
                     dataset_info = {"error": str(e)}
 
                 finally:
-                records_generated += 1
-                yield next_row
+                    records_generated += 1
+                    yield next_row
 
     def get_read_schema(self):
         """TBD"""
