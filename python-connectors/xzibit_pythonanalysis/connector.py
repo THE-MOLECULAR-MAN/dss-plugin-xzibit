afa9bddbc2d8023d4e6a213a69683348965bf69d
admin <admin>
2026-01-27 13:42:14 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -130,7 +130,7 @@ class ConnectorPythonAnalysis(Connector):
     def _analyze_radon(self, code: str) -> Dict[str, Any]:
         """Run Radon for Cyclomatic Complexity and Maintainability Index."""
         try:
-            logger.warning(f"_analyze_radon started")
+            logger.info(f"_analyze_radon started")
             # invalid syntax (<unknown>, line 1)
             # Cyclomatic Complexity
             complexity = radon_cc.cc_visit(code)
