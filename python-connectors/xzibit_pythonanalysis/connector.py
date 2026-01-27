a87fc4bb8cae990a53643834d823e8297a2cda96
admin <admin>
2026-01-27 11:26:57 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -209,7 +209,7 @@ class ConnectorPythonAnalysis(Connector):
         records_generated = 0
 
         # iterate through each project
-        for project_key in self.__client.list_projectskeys():
+        for project_key in self.__client.list_project_keys():
             if records_limit > 0 and records_generated >= records_limit:
                 return
             try:
