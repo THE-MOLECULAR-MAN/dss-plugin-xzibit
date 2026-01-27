7eb6bb7bc58f6fe10b28ec6db279e7a86fe644d5
admin <admin>
2026-01-26 21:58:55 -0500

    Edited file 'python-connectors/xzibit_pluginusages/connector.py'

--- python-connectors/xzibit_pluginusages/connector.py
+++ python-connectors/xzibit_pluginusages/connector.py
@@ -65,6 +65,7 @@ class ConnectorPlugins(Connector):
                         next_row["object_id"] = dataset_handle.id
 
                         dataset_info = dataset_handle.get_info().get_raw()
+                        pp(dataset_info)
 
                         yield next_row
 
