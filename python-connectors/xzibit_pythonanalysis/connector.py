655aff90a3b58f93af3473960326f22e504109fd
admin <admin>
2026-01-27 11:35:29 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -17,6 +17,9 @@ import dataiku
 from dataiku import api_client
 from dataiku.connector import Connector
 
+
+from xzibit.utils import get_dss_base_url, pp, get_python_recipe_code_env
+
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger = logging.getLogger()
 
