a108d5259d397e87284fcff6ae9ce7f03ead2903
admin <admin>
2026-01-27 17:43:42 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -289,8 +289,8 @@ class ConnectorPythonAnalysis(Connector):
                             min_python_version_from_vermin = (
                                 str(mpvv[0]) + "." + str(mpvv[1])
                             )
-                            next_row["min_python_version_from_Vermin"] = (
-                                min_python_version_from_Vermin
+                            next_row["min_python_version_from_vermin"] = (
+                                min_python_version_from_vermin
                             )
 
                             # 2. Radon (Complexity) - WORKING
