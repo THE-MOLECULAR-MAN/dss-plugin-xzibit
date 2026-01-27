bc700fcacabf409de093a8ad47473fab40ed7cb3
admin <admin>
2026-01-27 09:43:57 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -83,7 +83,7 @@ class ConnectorPythonAnalysis(Connector):
                                 f"Error analyzing {name} in {project_key}: {e}"
                             )
                         finally:
-                            # Build the row
+                            # Re
                             records_generated += 1
                             yield next_row
 
