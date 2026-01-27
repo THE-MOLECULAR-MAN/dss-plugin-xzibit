b9f32f35f89a130b55ea8a396af112e2786a1689
admin <admin>
2026-01-27 13:12:59 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -258,7 +258,7 @@ class ConnectorPythonAnalysis(Connector):
                             # --- Analysis Tools ---
 
                             # 1. Vermin (Min Python Version)
-                            # not working, all rows return ""
+                            # not working, all rows return "Error"
                             next_row["vermin_min_version"] = self._analyze_vermin(code)
 
                             # # 2. Radon (Complexity)
