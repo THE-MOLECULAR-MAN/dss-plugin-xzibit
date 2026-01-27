1b2d8582fe061e9093b6915ffe84eba65245487a
admin <tim.honker@dataiku.com>
2026-01-26 20:47:27 -0500

    Edited files

--- python-connectors/xzibit_pluginusages/connector.py
+++ python-connectors/xzibit_pluginusages/connector.py
@@ -39,45 +39,6 @@ class ConnectorPlugins(Connector):
         """TBD"""
         records_generated = 0
 
-        # list_plugins does not offer any parameters
-        # list_plugins returns a list of dict. Each dict contains at least a ‘id’ field
-        for item_info in self.__client.list_plugins():
-            try:
-                if records_limit > 0 and records_generated >= records_limit:
-                    return
-
-                plugin_id = item_info.get("id", "NO_PLUGIN_ID")
-                plugin_handle = self.__client.get_plugin(plugin_id)
-                # settings_raw = plugin_handle.get_settings().get_raw()
-                list_of_usages = plugin_handle.list_usages()
-
-                for usage in list_of_usages.usages:
-                    try:
-                        if records_limit > 0 and records_generated >= records_limit:
-                            return
-
-                        next_row = {"plugin_id": plugin_id}
-                        next_row["plugin_is_deprecated"] = (
-                            plugin_id in DEPRECATED_PLUGIN_IDS
-                        )
-                        next_row["element_kind"] = usage.element_kind
-                        next_row["element_type"] = usage.element_type
-                        next_row["object_id"] = usage.object_id
-                        next_row["object_type"] = usage.object_type
-                        next_row["project_key"] = usage.project_key  # may not have one
-
-                    except Exception as e:
-                        print(
-                            f"[plugin_usages-generate_rows] ! [UNEXPECTED EXCEPTION] {e} with plugin {plugin_id}"
-                        )
-                    finally:
-                        records_generated += 1
-                        yield next_row
-            except Exception as e:
-                print(
-                    f"[plugin_usages-generate_rows] [UNEXPECTED EXCEPTION] {e} with plugin {plugin_id}"
-                )
-
     def get_read_schema(self):
         """TBD"""
         return None
