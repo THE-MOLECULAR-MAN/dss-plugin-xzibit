5d097bd5ff1b47edac8e07b1f7fc848980d9c7c8
admin <admin>
2026-01-27 09:42:11 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -72,7 +72,8 @@ class ConnectorPythonAnalysis(Connector):
                     if code:
                         next_row = {
                             "project_key": project_key,
-                            "recipe_name": name,}
+                            "recipe_name": name,
+                        }
 
                         # vermin analysis:
                         vermin_analysis = self.analyze_code_compatibility(code)
