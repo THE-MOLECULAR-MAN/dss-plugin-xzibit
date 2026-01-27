19f5d08586fce46c78244866313824b7c7c34977
admin <admin>
2026-01-27 13:21:47 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -103,6 +103,7 @@ class ConnectorPythonAnalysis(Connector):
         try:
             # Vermin expects a path or logic to parse. We use its internal detect.
             # detect returns (mins, parsable, text)
+            logger.info(f"Analyzing Python code for recipe {name}...")
             mins = detect(code, config=self.vermin_config).mins
             if mins:
                 # Returns something like "3.8"
