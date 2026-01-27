cc2b4c7cf07bb64730b86ac0f6d16ba10e3d65a6
admin <admin>
2026-01-27 11:24:24 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -220,7 +220,7 @@ class ConnectorPythonAnalysis(Connector):
                 python_recipes = self.get_python_recipes(project_handle)
 
                 for recipe_meta in python_recipes:
-                    logger.info(f"starting on project {project_key}")
+                    logger.info(f"Analyzi {project_key}")
 
                     name = recipe_meta["name"]
                     code = self.get_recipe_code(name)
