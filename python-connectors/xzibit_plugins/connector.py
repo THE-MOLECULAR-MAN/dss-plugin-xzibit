fac82c804e0285d86ab2d65f00ee87cd44196084
admin <admin>
2026-01-27 07:48:15 -0500

    Edited file 'python-connectors/xzibit_plugins/connector.py'

--- python-connectors/xzibit_plugins/connector.py
+++ python-connectors/xzibit_plugins/connector.py
@@ -97,6 +97,7 @@ class ConnectorPlugins(Connector):
 
                 plugin_handle = self.__client.get_plugin(plugin_id)
                 settings_raw = plugin_handle.get_settings().get_raw()
+                pp()
                 next_row["code_env_name"] = settings_raw.get("codeEnvName", None)
 
                 # TODO: replace with CSV file method
