539e06abd02f2e9b4e9202ed1d4fcec4d202f2d4
admin <admin>
2026-01-26 22:14:54 -0500

    Edited file 'python-connectors/xzibit_pluginusages/connector.py'

--- python-connectors/xzibit_pluginusages/connector.py
+++ python-connectors/xzibit_pluginusages/connector.py
@@ -86,6 +86,7 @@ class ConnectorPlugins(Connector):
                     # determine if recipe uses a plugin
                     recipe_settings_handle = recipe_handle.get_settings()
                     raw_data = recipe_settings_handle.get_recipe_raw_definition()
+                    raw_data.get("type", "")
 
                     print("Recipe Raw Data:")
                     pp(raw_data)
