cc7ec7af22981c021a06092236e9fcfcdc8f1f41
admin <admin>
2026-01-27 08:41:22 -0500

    Edited file 'python-connectors/xzibit_pluginusages/connector.py'

--- python-connectors/xzibit_pluginusages/connector.py
+++ python-connectors/xzibit_pluginusages/connector.py
@@ -7,9 +7,7 @@ from dataiku import api_client
 from dataiku.connector import Connector
 from xzibit.utils import (
     get_dss_base_url,
-    flatten_dict,
-    remove_prefix_from_keys,
-    list_to_error_dict,
+    
     pp,
     jd,
     get_values_for_key,
