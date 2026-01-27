51876d20b81fe96f4822df9da1a7e222a33ea14d
admin <admin>
2026-01-27 18:27:42 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -189,6 +189,7 @@ class ConnectorPythonAnalysis(Connector):
 
         try:
             # Run the command against the temp file
+            lo
             result = subprocess.run(
                 cmd + [tmp_path],
                 capture_output=True,
