fe0bfc5d608caac105a6796faefec2e1e865174d
admin <admin>
2026-01-26 21:33:00 -0500

    Edited file 'python-connectors/xzibit_pluginusages/connector.py'

--- python-connectors/xzibit_pluginusages/connector.py
+++ python-connectors/xzibit_pluginusages/connector.py
@@ -94,7 +94,7 @@ class ConnectorPlugins(Connector):
                         }
 
                         next_row["object_id"] = recipe_handle.id
-                        nex
+                        next_row
 
                         # recipe_info = recipe_handle.get_definition()
 
