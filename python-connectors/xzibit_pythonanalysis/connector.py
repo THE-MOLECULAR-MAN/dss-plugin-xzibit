fde2007df6db8bb32bbcde024b1dc7c5ae547b9a
admin <admin>
2026-01-27 09:39:24 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -36,7 +36,6 @@ class ConnectorPythonAnalysis(Connector):
         """
         Fetches the actual Python script content from a specific recipe.
         """
-
         try:
             recipe = self.project.get_recipe(recipe_name)
             settings = recipe.get_settings()
