049893defe7f72a1017d86556c06679f6223de53
admin <admin>
2026-01-27 09:42:43 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -70,6 +70,7 @@ class ConnectorPythonAnalysis(Connector):
                     code = self.get_recipe_code(name)
 
                     if code:
+                        try:
                         next_row = {
                             "project_key": project_key,
                             "recipe_name": name,
