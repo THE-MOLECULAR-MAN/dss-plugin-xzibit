e2f9905ea3e4548ae2b224e97ac4d224eb979e31
admin <admin>
2026-01-27 17:53:55 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -236,6 +236,7 @@ class ConnectorPythonAnalysis(Connector):
             output = self._run_subprocess_tool(
                 ["ruff", "check", "--output-format=json"], code
             )
+            logger.info(f"Analyzing Python code for recipe {name}...")
             data = json.loads(output)
             return len(data)
         except Exception:
