223a1520f68cc93c528fed67328bbfe4193f3019
admin <admin>
2026-01-27 23:58:59 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -301,7 +301,7 @@ class ConnectorPythonAnalysis(Connector):
                             recipe_handle
                         )
                         
-                        row["last_modified_timestamp"] = get_recipe_last_modifier_user(
+                        row["last_modified_timestamp"] = 
 
                         # Run In-Memory Analysis immediately (Vermin, Radon)
                         mpvv = self._analyze_vermin(code)
