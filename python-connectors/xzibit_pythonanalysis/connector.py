262466f6c2d796aa5385efdebf879079fc2a6d87
admin <admin>
2026-01-28 08:15:30 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -128,7 +128,7 @@ class ConnectorPythonAnalysis(Connector):
     def _is_default_code_recipe(self, code) -> bool:
         """Check if the recipe code matches the default template."""
         default_code_contains =[
-            ""
+            "_df = ... # Compute a Pandas dataframe to write into "
         ]
         
         try:
