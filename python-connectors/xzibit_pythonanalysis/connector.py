7fb1d257b5dba89ad9051592c20ba583138e5818
admin <admin>
2026-01-27 23:16:20 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -192,7 +192,7 @@ class ConnectorPythonAnalysis(Connector):
         except Exception as e:
             # Log the error as per your existing structure
             logger.error(f"Radon analysis failed: {e}")
-            
+            print(code)
             return {
                 "radon_cc_avg": -1,
                 "radon_mi_score": -1,
