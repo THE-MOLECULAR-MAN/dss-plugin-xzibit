f9c830c625e86723d58ae2753544a530395035aa
admin <admin>
2026-01-27 23:03:19 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -9,7 +9,8 @@ import ast
 import subprocess
 from typing import Dict, List, Any, Tuple
 
-# from vermin import detect, Config
+from vermin import detect, Config
+
 # import radon.complexity as radon_cc
 # import radon.metrics as radon_mi
 
