2764455426dc4257d11480cb1ef3cc8d4bbfba7b
admin <admin>
2026-01-27 23:27:54 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -63,7 +63,9 @@ class ConnectorPythonAnalysis(Connector):
 
     def get_url(self, id, project_key):
         """Create a URL to the DSS object in question in this specific DSS instance.
-        Return None if any of the inputs are None."""
+        Return None if any of the inputs are None.
+        
+        May be different one day"""
         # at least one is None, return None
         if any(v is None for v in (self.__baseurl, id, project_key)):
             return None
