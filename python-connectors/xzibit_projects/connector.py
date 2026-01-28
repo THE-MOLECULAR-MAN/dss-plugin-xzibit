c1ba4f9f53ab81384646674ecf389af35d858d6c
admin <admin>
2026-01-28 00:01:20 -0500

    Edited file 'python-connectors/xzibit_projects/connector.py'

--- python-connectors/xzibit_projects/connector.py
+++ python-connectors/xzibit_projects/connector.py
@@ -6,7 +6,7 @@
 from dataiku import api_client
 from dataiku.connector import Connector
 
-from 
+from xz
 ####################################################################
 # Unique imports for this Class
 ####################################################################
