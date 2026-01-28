e6b5e845ecbbbc4872120118515b74d4c1be584d
admin <admin>
2026-01-28 08:08:12 -0500

    Edited file 'python-lib/xzibit/default_py_code_recipe.py'

---
+++ python-lib/xzibit/default_py_code_recipe.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+INPUT_PLACEHOLDER = dataiku.Dataset("INPUT_PLACEHOLDER")
+INPUT_PLACEHOLDER_df = INPUT_PLACEHOLDER.get_dataframe()
+
+
+# Write recipe outputs
+OUTPUT_PLACEHOLDER = dataiku.Folder("PLACEHOLDER3")
+OUTPUT_PLACEHOLDER_info = OUTPUT_PLACEHOLDER.get_info()
