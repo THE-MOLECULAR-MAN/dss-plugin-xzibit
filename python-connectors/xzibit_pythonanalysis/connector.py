0bc635fca4ada100042b2aa0a3ee12111790d462
admin <admin>
2026-01-27 23:11:51 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -179,8 +179,8 @@ class ConnectorPythonAnalysis(Connector):
                 h_volume=halstead_metrics.total.volume,
                 comments=raw_metrics.comments,
             )
-            
-            logger.error(f"_analyze_radon 2")
+
+            logger.error(f"_analyze_radon end")
 
             return {
                 "radon_cc_avg": round(avg_complexity, 2),
