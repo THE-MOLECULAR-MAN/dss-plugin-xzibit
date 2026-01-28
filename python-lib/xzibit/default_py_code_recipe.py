622a14ce4972350a1183e680b97d9a413570cff4
admin <admin>
2026-01-28 08:08:22 -0500

    Edited file 'python-lib/xzibit/default_py_code_recipe.py'

--- python-lib/xzibit/default_py_code_recipe.py
+++ python-lib/xzibit/default_py_code_recipe.py
@@ -8,8 +8,6 @@ INPUT_PLACEHOLDER = dataiku.Dataset("INPUT_PLACEHOLDER")
 INPUT_PLACEHOLDER_df = INPUT_PLACEHOLDER.get_dataframe()
 
 
-
-
 # Write recipe outputs
 OUTPUT_PLACEHOLDER = dataiku.Folder("PLACEHOLDER3")
 OUTPUT_PLACEHOLDER_info = OUTPUT_PLACEHOLDER.get_info()
