5e61997d588c16ca8ca02e220b9d3b852ab7ce4e
admin <admin>
2026-01-28 08:09:18 -0500

    Edited file 'python-lib/xzibit/python_code_recipe_template.py'

--- python-lib/xzibit/python_code_recipe_template.py
+++ python-lib/xzibit/python_code_recipe_template.py
@@ -4,13 +4,14 @@ import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 
+
 # Compute recipe outputs
 # TODO: Write here your actual code that computes the outputs
 # NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
 
-TEMPLATE_df = ...  # Compute a Pandas dataframe to write into TEMPLATE
+unaltered_default_python_code_recipe_df = ... # Compute a Pandas dataframe to write into unaltered_default_python_code_recipe
 
 
 # Write recipe outputs
-TEMPLATE = dataiku.Dataset("TEMPLATE")
-TEMPLATE.write_with_schema(TEMPLATE_df)
+unaltered_default_python_code_recipe = dataiku.Dataset("unaltered_default_python_code_recipe")
+unaltered_default_python_code_recipe.write_with_schema(unaltered_default_python_code_recipe_df)
