9b5dc58569ee56ba98b6f584866a33131324e7a9
admin <admin>
2026-01-28 08:14:07 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -125,7 +125,7 @@ class ConnectorPythonAnalysis(Connector):
     # In-Memory Analysis Tools (Fast)
     # -------------------------------------------------------------------------
     
-    def 
+    def _
 
     def _analyze_vermin(self, code: str) -> str:
         """Run Vermin to detect minimum Python version."""
