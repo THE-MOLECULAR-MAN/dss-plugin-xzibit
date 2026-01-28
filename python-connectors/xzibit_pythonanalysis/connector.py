3d5151515ae4050d40dc98141a4b01fe82a820f4
admin <admin>
2026-01-27 22:52:45 -0500

    Edited file 'python-connectors/xzibit_pythonanalysis/connector.py'

--- python-connectors/xzibit_pythonanalysis/connector.py
+++ python-connectors/xzibit_pythonanalysis/connector.py
@@ -291,7 +291,7 @@ class ConnectorPythonAnalysis(Connector):
                             )
                             next_row["code_env_name"] = code_env_name
                             next_row["code_env_python_version"] = python_version
-                            next_row["num_lines"] = python_version
+                            next_row["num_lines"] = len(code.splitlines())
 
                             # --- Analysis Tools ---
 
