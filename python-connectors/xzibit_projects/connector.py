7fd40a82a2ab4ea7b70728dc21a66ba8c7a8a032
admin <admin>
2026-01-29 07:48:35 -0500

    Edited file 'python-connectors/xzibit_projects/connector.py'

--- python-connectors/xzibit_projects/connector.py
+++ python-connectors/xzibit_projects/connector.py
@@ -6,10 +6,7 @@
 from dataiku import api_client
 from dataiku.connector import Connector
 
-from xzibit.utils import (
-    flatten_dict,
-    remove_prefix_from_keys,
-)
+
 ####################################################################
 # Unique imports for this Class
 ####################################################################
